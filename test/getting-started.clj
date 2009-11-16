2f333bbf8df35fb0bf1c4128b9b426772e1a5670
Sam Aaron <samaaron@gmail.com>
2009-11-16 20:07:19 +0100

    add getting started to test dir

---
+++ test/getting-started.clj
@@ -0,0 +1,6 @@
+(use 'overtone.sc)
+(boot)
+(hit) ; makes a test noise
+(hit (now) "kick") ; hits the kick drum right now
+(hit (+ (now) 1000) "kick") ; hits the kick drum in 1 second (1,000 ms)
+(quit)
