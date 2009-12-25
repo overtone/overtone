5934d418c6c516e023884708dfb6fb9e1c545023
Fabian Aussems <fabianaussems@gmail.com>
2009-12-25 14:06:42 +0100

    changed scripts

---
+++ connect.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+jack_connect SuperCollider:out_1 system:playback_1
+jack_connect SuperCollider:out_2 system:playback_2
+
--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 killall -9 jackd
-jackd -t 5000 -d alsa --rate 44100 --softmode --inchannels 0 --outchannels 2 &
+jackd -t 5000 -d alsa --rate 44100 --softmode --inchannels 2 --outchannels 2 &
 echo "play" | jack_transport
-#jack_connect system:capture_1 system:playback_1
-#jack_connect system:capture_2 system:playback_2
 
-killall -9 pulseaudio
-pulseaudio -nF ~/.jack.pa --start
+
+#killall -9 pulseaudio
+#pulseaudio -nF ~/.jack.pa --start
