927ae5e8d9b72886744fba030a120404a69ff854
Fabian Aussems <fabianaussems@gmail.com>
2009-12-25 18:26:51 +0100

    removed pulseaudio stuff

--- start.sh
+++ start.sh
@@ -3,6 +3,3 @@
 killall -9 jackd
 jackd -t 5000 -d alsa --rate 44100 --softmode --inchannels 2 --outchannels 2 &
 echo "play" | jack_transport
-
-killall -9 pulseaudio
-pulseaudio -nF ~/.jack.pa --start
