f9ce188d140fc2d29b31d1708f47d76274209aa4
Fabian Aussems <fabianaussems@gmail.com>
2009-12-25 14:09:27 +0100

    pulseaudio to jack

--- start.sh
+++ start.sh
@@ -4,6 +4,5 @@ killall -9 jackd
 jackd -t 5000 -d alsa --rate 44100 --softmode --inchannels 2 --outchannels 2 &
 echo "play" | jack_transport
 
-
-#killall -9 pulseaudio
-#pulseaudio -nF ~/.jack.pa --start
+killall -9 pulseaudio
+pulseaudio -nF ~/.jack.pa --start
