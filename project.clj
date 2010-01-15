7f78253427aa97d83b01bc1e582c3a995dd2eefc
Jeff Rose <rosejn@gmail.com>
2010-01-15 23:44:44 +0100

    Extracting byte-spec as a submodule and grabbing tests for other modules.

--- project.clj
+++ project.clj
@@ -3,11 +3,13 @@
   :repositories [["java.net" "http://download.java.net/maven/2/"]]
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-		 [org.clojars.fyuryu/rosado.processing "1.0.7"]
                  [net.java.dev.scenegraph/scenegraph "svn"]
-		 [org.clojars.rosejn/jvi "0.7.1"]
+                 [org.clojars.rosejn/jvi "0.7.1"]
                  [jfree/jfreechart "1.0.12"]
+                 [osc-clj "0.1"]
+                 [byte-spec "0.1"]
+                 [midi-clj "0.1"]
                 ]
   :dev-dependencies [[swank-clojure "1.1.0-SNAPSHOT"]
                      [lein-clojars "0.5.0-SNAPSHOT"]]
-  :main overtone.gui.main)
+  :main overtone.studio.gui.main)
