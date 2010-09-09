8ddbf91648477528ae28741e157335fc4fa9a91d
Sam Aaron <samaaron@gmail.com>
2010-09-09 10:27:42 +0100

    modify apply-at to apply the fn at the specified time and create apply-before which will apply the fn slightly before the specified time

--- src/overtone/core/time_utils.clj
+++ src/overtone/core/time_utils.clj
@@ -67,12 +67,15 @@
 ; the instance of the function defined earlier.
 ; (apply-at #'my-melody (+ dur (now)) arg1 arg2)
 
-(def *APPLY-AHEAD* 50)
+(def *APPLY-AHEAD* 250)
 
 (defn apply-at [func ms-time & args]
-  (let [delay-time (- ms-time *APPLY-AHEAD* (now))]
+  (let [delay-time (- ms-time (now))]
     (if (< delay-time 0)
       (apply func args)
       (schedule #(apply func args) delay-time))))
+
+(defn apply-before [func ms-time & args]
+  (apply apply-at func (- ms-time *APPLY-AHEAD*) args))
 
 
