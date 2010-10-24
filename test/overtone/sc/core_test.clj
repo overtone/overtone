1c55515c1c5a40532e72c893fb192e3f8c0d8b28
Jeff Rose <rosejn@gmail.com>
2010-10-24 17:11:42 +0200

    adding moved unit tests

---
+++ test/overtone/sc/core_test.clj
@@ -0,0 +1,67 @@
+(ns overtone.sc.core-test
+  (:use
+    clojure.test
+    overtone.core
+    overtone.music.instrument.synth)
+  (:require
+    [overtone.core.log :as log]))
+
+(def ditty-notes [50 50 57 50 48 62 62 50])
+(def ditty-durs  [250 250 500 125 125 250 250 500])
+
+(defn play-seqs [inst notes durs]
+  (loop [notes notes
+         durs durs
+         t (now)]
+    (when (and notes durs)
+      (hit t inst :note (first notes) :dur (first durs))
+      (recur (next notes) (next durs) (+ t (first durs))))))
+
+(deftest boot-test
+  (try
+    (boot)
+    (Thread/sleep 500)
+    (is (not (nil? @server*)))
+    (is (= 1 (:n-groups (status))))
+
+    (play-seqs "sin" ditty-notes ditty-durs)
+    (Thread/sleep 3000)
+    (finally
+      (quit))))
+
+(def DEFAULT-GROUP 1)
+
+(deftest groups-test
+  (let [a (group :head DEFAULT-GROUP)
+        b (group :head DEFAULT-GROUP)
+        c (group :head DEFAULT-GROUP)]
+    (is (= 4 (:n-groups (status))))
+    (group-free a b c)
+    (is (= 1 (:n-groups (status))))
+    ; We should get the old ID again with the bitset allocator
+    (is (= a (group :head DEFAULT-GROUP)))))
+
+(deftest node-tree-test
+  (reset)
+  (let [g1 (group :head 0)
+        g2 (group :tail 0)]
+    (hit :sin :dur 2000 :target g2)
+    (Thread/sleep 100)
+    (is (= 1 (:n-synths (status))))))
+
+; These are what the responses look like for a queryTree msg.  The first
+; without and the second with control information.
+(def no-ctls [0 0 2 1 2 2 0 3 0 1001 -1 "sin"])
+(def with-ctls [1 0 2 1 2 2 0 3 0 1001 -1 "sin" 3 "out" 0.0 "pitch" 40.0 "dur" 100000.0])
+
+(defn test-ns-hook []
+  (try
+    (boot-test)
+    (boot)
+    (Thread/sleep 500)
+    (groups-test)
+    (node-tree-test)
+    (reset)
+    (finally
+      (quit))))
+
---
+++ test/overtone/sc/synthdef_test.clj
@@ -0,0 +1,86 @@
+(ns overtone.sc.synthdef-test
+  (:import (java.io FileInputStream FileOutputStream
+                    DataInputStream DataOutputStream
+                    BufferedInputStream BufferedOutputStream
+                    ByteArrayOutputStream ByteArrayInputStream))
+  (:use overtone.core
+        byte-spec
+        test-utils
+        clojure.test)
+  (:require [overtone.log :as log]))
+
+(defn sawzall-raw
+  []
+  {:name "sawzall"
+   :n-constants (short 1)
+   :constants [(float 0.0)]
+   :n-params  (short 1)
+   :params    [(float 50.0)]
+   :n-pnames  (short 1)
+   :pnames    [{:index (short 0), :name "note"}]
+   :n-ugens   (short 4)
+   :ugens     [{:outputs [{:rate (byte 1)}], :inputs [], :special (short 0), :n-outputs (short 1), :n-inputs (short 0), :rate (short 1), :name "Control"}
+
+               {:outputs [{:rate (byte 1)}], :inputs [{:index (short 0), :src (short 0)}], :special (short 17),
+                :n-outputs (short 1), :n-inputs (short 1), :rate (byte 1), :name "UnaryOpUGen"}
+
+               {:outputs [{:rate (byte 2)}], :inputs [{:index (short 0), :src (short 1)}], :special (short 0),
+                :n-outputs (short 1), :n-inputs (short 1), :rate (byte 2), :name "Saw"}
+
+               {:outputs [], :inputs [{:index (short 0), :src (short -1)} {:index (short 0), :src (short 2)}],
+                :special (short 0), :n-outputs (short 0), :n-inputs (short 2), :rate (byte 2), :name "Out"}]
+   :n-variants (short 0)
+   :variants []})
+
+(deftest self-consistent-syndef
+  (let [a (sawzall-raw)
+        b (bytes-and-back synth-spec a)]
+    (is (= a b))))
+
+(def mini-sin (synthdef "mini-sin" {"freq" 440}
+                        (out:ar 0 (sin-osc:ar 0))))
+
+(deftest native-synth-test
+  (let [bytes (synthdef-bytes mini-sin)
+        synth (synthdef-read bytes)
+        ugens (:ugens synth)
+        [control sin out] ugens]
+    (is (= 1 (:n-constants synth)))
+    (is (= [0.0] (:constants synth)))
+    (is (= 1 (:n-params synth)))
+    (is (= 3 (:n-ugens synth)))
+    (is (= 3 (count (:ugens synth))))
+    (is (= "Control" (:name control)))
+    (is (= "SinOsc" (:name sin)))
+    (is (= "Out" (:name out)))
+    (is (= 1 (:rate control)))
+    (is (= 2 (:rate sin)))
+    (is (= 2 (:rate out)))
+    (is (= 2 (:n-inputs sin)))
+    (is (= 2 (:n-inputs out)))
+    (is (= 1 (:n-outputs sin)))
+    (is (= 0 (:n-outputs out)))
+    (is (= {:src 0 :index 0} (first (:inputs sin))))
+    (is (= {:src -1 :index 0} (second (:inputs sin))))
+    (is (= {:src -1, :index 0} (first (:inputs out))))
+    (is (= {:src 1, :index 0} (second (:inputs out))))
+    ))
+
+(def TOM-DEF "test/data/tom.scsyndef")
+(def KICK-DEF "test/data/round-kick.scsyndef")
+
+(defn rw-file-test [path]
+  (let [a (synthdef-read path)
+        b (bytes-and-back synth-spec a)]
+    (is (= a b))))
+
+(deftest read-write-test []
+  (rw-file-test TOM-DEF)
+  (rw-file-test KICK-DEF))
+
+(deftest zzz-test []
+  (log/info "Active Threads: " (Thread/activeCount)))
+
+(defn synthdef-tests []
+  (binding [*test-out* *out*]
+    (run-tests 'synthdef-test)))
