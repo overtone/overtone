4379bfe68a48dbeac8df1674930314f7dd85355f
Jeff Rose <rosejn@gmail.com>
2010-12-19 18:19:09 +0100

    adding some more default arg strings

--- src/overtone/sc/ugen/defaults.clj
+++ src/overtone/sc/ugen/defaults.clj
@@ -24,7 +24,15 @@
 (def NO-ARG-DOC-FOUND "-")
 
 (def DEFAULT-ARG-DOCS
-  {"bufnum" "The index of the buffer to use."})
+  {"bufnum" "A buffer or buffer index value."
+   "freq" "frequency in hz (cycles per second)"
+   "freq1" "frequency in hz (cycles per second)"
+   "freq2" "frequency in hz (cycles per second)"
+   "freq3" "frequency in hz (cycles per second)"
+   "phase" "the start point within a cycle"
+   "loop" "a boolean switch to turn on looping"
+   "in" "the input signal"
+   })
 
 (def DOC-WIDTH 60)
 
