1efea9f07320a8cf440796c612ccb3d4b32015eb
Sam Aaron <samaaron@gmail.com>
2011-04-09 15:28:40 +0100

    Add mda-piano ugen (this currently doesn't exist in the scsynth-interop ugen distribution yet, however hopefully it will somepoint soon).

--- src/overtone/sc/ugen.clj
+++ src/overtone/sc/ugen.clj
@@ -307,7 +307,7 @@
 (def UGEN-NAMESPACES
   '[basicops buf-io compander delay envgen fft2 fft-unpacking grain
     io machine-listening misc osc beq-suite chaos control demand
-    ff-osc fft info noise pan trig line input filter random])
+    ff-osc fft info noise pan trig line input filter random mda])
 
 
 
