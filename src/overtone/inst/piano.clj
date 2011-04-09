7f246bc2b925a427358f1fd4bb5c57f541804cdd
Sam Aaron <samaaron@gmail.com>
2011-04-09 17:02:16 +0100

    Add piano instrument based on mda-piano

---
+++ src/overtone/inst/piano.clj
@@ -0,0 +1,37 @@
+(ns overtone.inst.piano
+  (:use overtone.live))
+
+;; IMPORTANT: requires the mda-piano ugen to be available on your system
+
+(definst piano [note 60
+                 gate 1
+                 vel 100
+                 decay 0.8
+                 release 0.8
+                 hard 0.8
+                 velhard 0.8
+                 muffle 0.8
+                 velmuff 0.8
+                 velcurve 0.8
+                 stereo 0.2
+                 tune 0.5
+                 random 0.1
+                 stretch 0.1
+                 sustain 0.1]
+  (let [snd (mda-piano :freq (midicps note)
+                       :gate gate
+                       :vel vel
+                       :decay decay
+                       :release release
+                       :hard hard
+                       :velhard velhard
+                       :muffle muffle
+                       :velmuff velmuff
+                       :velcurve velcurve
+                       :stereo stereo
+                       :tune tune
+                       :random random
+                       :stretch stretch
+                       :sustain sustain)
+        sil (detect-silence snd 0.001 :action :free)]
+    (* 1 snd))) ;;TODO: figure out why this mul is required
