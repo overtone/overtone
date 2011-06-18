a39d2aed8a1f603f86609791bdd31fb20317a3f0
Sam Aaron <samaaron@gmail.com>
2011-06-18 18:17:20 +0100

    place :pos before :target in synth calls to match the ordering in group

--- src/overtone/sc/node.clj
+++ src/overtone/sc/node.clj
@@ -77,7 +77,7 @@
   (node \"foo\")
   (node \"foo\" {:pitch 60})
   (node \"foo\" {:pitch 60} {:target 0})
-  (node \"foo\" {:pitch 60} {:target 2 :position :tail})
+  (node \"foo\" {:pitch 60} {:position :tail :target 2})
   "
   ([synth-name arg-map] (node synth-name arg-map {:position :tail, :target 0}))
   ([synth-name arg-map location]
