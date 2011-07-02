eaec714c50bf5ccef455923af1b7337c11c6e160
Sam Aaron <samaaron@gmail.com>
2011-07-02 18:15:28 +0100

    Docstring util commit seemed to get clobbered by popping a previous stash :-(

--- src/overtone/doc_util.clj
+++ src/overtone/doc_util.clj
@@ -20,7 +20,8 @@
   "Generates a padding string starting concatting s with len times pad:
    (gen-padding \"\" 5 \"b\") ;=> \"bbbbb\"
    May be called without starting string s in which case it defaults to the
-   empty string."
+   empty string and also without pad in which case it defaults to a single space"
+  ([len] (gen-padding "" len " "))
   ([len pad] (gen-padding "" len pad))
   ([s len pad]
      (if (> len 0)
@@ -30,17 +31,26 @@
 (defn indented-str-block
   "Appends a list ls of strings to string s in a formatted block with a specific
    width max-len and indentation indent. May be called with a basic text string
-   and max-len and indent in which case text will be split on whitespace."
+   and max-len and indent in which case text will be split on whitespace.
+
+  Will clobber all single \n found but will honour two in a row. This allows
+  docstrings to contain \n for formatting purposes in source form rather than
+  require them to exist on one large line."
   ([txt max-len indent] (let [id (str (java.util.UUID/randomUUID))
                               split-text (replace-re #"[\n]{2}" id txt)
                               split-text (replace-str "\n" " " split-text)
-                              split-text (replace-str id "\n" split-text)
+                              split-text (replace-str id "\n\n " split-text)
                               split-text (split #" +" split-text)]
                           (indented-str-block "" split-text 0 max-len indent)))
   ([s ls cur-len max-len indent]
      (if (empty? ls)
        s
        (let [f-len (.length (first ls))]
-         (if (> (+ cur-len f-len) max-len)
-           (indented-str-block (str s "\n" (gen-padding "" indent " ")) ls 0 max-len indent)
-           (indented-str-block (str s (first ls) " ") (rest ls) (+ cur-len f-len) max-len indent))))))
+         (if (.endsWith (first ls) "\n\n")
+           (if (> (+ cur-len f-len) max-len)
+             (indented-str-block (str s "\n" (gen-padding indent) (first ls) (gen-padding indent)) (rest ls) 0 max-len indent)
+             (indented-str-block (str s (first ls) (gen-padding indent)) (rest ls) 0 max-len indent))
+           (if (> (+ cur-len f-len) max-len)
+             (indented-str-block (str s "\n" (gen-padding indent)) ls 0 max-len indent)
+             (indented-str-block (str s (first ls) " ") (rest ls) (+ cur-len f-len) max-len indent))
+           )))))
