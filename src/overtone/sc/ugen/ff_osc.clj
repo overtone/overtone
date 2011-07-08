fbb27a10fed5b3535918a34fbbf5a70fa086a2c7
Sam Aaron <samaaron@gmail.com>
2011-07-08 13:03:39 +0100

    Fix up klank and klang ugen init fns to munge the specifications correctly. Also some slight doc tweaking.

--- src/overtone/sc/ugen/ff_osc.clj
+++ src/overtone/sc/ugen/ff_osc.clj
@@ -31,19 +31,20 @@
       ;; }
 
       {:name "Klang",
-       :args [{:name "specs", :mode :not-expanded :doc "An array of three arrays frequencies, amplitudes and phases:  1) an array of filter frequencies, 2) an Array of filter amplitudes, or nil. If nil, then amplitudes default to 1.0, 3) an Array of initial phases, or nil. If nil, then phases default to 0.0."}
+       :args [{:name "specs", :mode :append-sequence :doc "An array of three arrays frequencies, amplitudes and phases:  1) an array of filter frequencies, 2) an Array of filter amplitudes, or nil. If nil, then amplitudes default to 1.0, 3) an Array of initial phases, or nil. If nil, then phases default to 0.0."}
               {:name "freqscale", :default 1.0 :doc "a scale factor multiplied by all frequencies at initialization time."}
               {:name "freqoffset", :default 0.0 :doc "an offset added to all frequencies at initialization time."}],
        :rates #{:ar}
-       :init (fn [rate [specs & args]]
-               (let [[freqs amps phases] specs
-                     amps (or amps (repeat 1.0))
-                     phases (or phases (repeat 0.0))
-                     faps (map vector freqs amps phases)]
-                 (apply concat args faps)))
+       :init (fn [rate args spec]
+               (let [[[ freqs amps times]] args
+                     amps                  (or amps (repeat (count freqs) 1.0))
+                     times                 (or times (repeat (count freqs) 1.0))
+                     fats                  (map vector freqs amps times)
+                     new-args              (concat [fats] (rest args))]
+                 new-args))
        :doc "Klang is a bank of fixed frequency sine oscillators. Klang is more efficient than creating individual oscillators but offers less flexibility.
 
-  The parameters in specificationsArrayRef can't be changed after it has been started.
+  The specs can't be changed after it has been started.
   For a modulatable but less efficient version, see dyn-klang."}
 
       ;; Klank : UGen {
@@ -68,28 +69,29 @@
       ;; }
 
       {:name "Klank",
-       :args [{:name "specs", :mode :not-expanded :doc "An array of three arrays: frequencies, amplitudes and ring times: *all arrays should have the same length*  1) an Array of filter frequencies. 2)  an Array of filter amplitudes, or nil. If nil, then amplitudes default to 1.0 3) an Array of 60 dB decay times for the filters."}
+       :args [{:name "specs", :mode :append-sequence :doc "An array of three arrays: frequencies, amplitudes and ring times: *all arrays should have the same length*  1) an Array of filter frequencies. 2)  an Array of filter amplitudes, or nil. If nil, then amplitudes default to 1.0 3) an Array of 60 dB decay times for the filters."}
               {:name "input" :doc "the excitation input to the resonant filter bank."}
               {:name "freqscale", :default 1.0 :doc "a scale factor multiplied by all frequencies at initialization time."}
               {:name "freqoffset", :default 0.0 :doc "an offset added to all frequencies at initialization time."}
               {:name "decayscale", :default 1.0 :doc "a scale factor multiplied by all ring times at initialization time."}],
        :rates #{:ar}
-       :init (fn [rate [specs & args]]
-               (let [[freqs amps times] specs
-                     amps               (or amps (repeat 1.0))
-                     times              (or times (repeat 1.0))
-                     fats               (map vector freqs amps times)]
-                 (apply concat args fats)))
+       :init (fn [rate args spec]
+               (let [[[ freqs amps times]] args
+                     amps                  (or amps (repeat (count freqs) 1.0))
+                     times                 (or times (repeat (count freqs) 1.0))
+                     fats                  (map vector freqs amps times)
+                     new-args              (concat [fats] (rest args))]
+                 new-args))
        :doc "Klank is a bank of fixed frequency resonators which can be used to simulate the resonant modes of an object. Each mode is given a ring time, which is the time for the mode to decay by 60 dB.
 
-  The parameters in specificationsArrayRef can't be changed after it has been started.
+  The specs can't be changed after it has been started.
   For a modulatable but less efficient version, see dyn-klank."}
 
       {:name "Blip",
        :args [{:name "freq", :default 440.0 :doc "Frequency in Hertz (control rate)"}
               {:name "numharm", :default 200.0 :doc "Number of harmonics. This may be lowered internally if it would cause aliasing."}],
        :rates #{:ar}
-       :doc "Band Limited ImPulse generator. All harmonics have equal amplitude. This is the equivalent of buzz in MusicN languages.
+       :doc "Band Limited Impulse generator. All harmonics have equal amplitude. This is the equivalent of buzz in MusicN languages.
   WARNING: This waveform in its raw form could be damaging to your ears at high amplitudes or for long periods.
 
   It is improved from other implementations in that it will crossfade in a control period when the number of harmonics changes, so that there are no audible pops. It also eliminates the divide in the formula by using a 1/sin table (with special precautions taken for 1/0).  The lookup tables are linearly interpolated for better quality.
