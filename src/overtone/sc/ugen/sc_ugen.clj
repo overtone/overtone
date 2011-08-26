4377b3f2bd767206f4810938f4a72f2832b10dbb
Sam Aaron <samaaron@gmail.com>
2011-08-26 16:40:59 +0100

    Add checks for nil args in control-proxy sc-ugen and output-proxy

--- src/overtone/sc/ugen/sc_ugen.clj
+++ src/overtone/sc/ugen/sc_ugen.clj
@@ -1,5 +1,6 @@
 (ns overtone.sc.ugen.sc-ugen
-  (:use [overtone.sc.ugen defaults]))
+  (:use [overtone util]
+        [overtone.sc.ugen defaults]))
 
 (defrecord SCUGen [id name rate rate-name special args n-outputs])
 (derive SCUGen ::sc-ugen)
@@ -7,9 +8,17 @@
 (defn sc-ugen? [obj] (isa? (type obj) ::sc-ugen))
 
 (defn sc-ugen
-  "Create a new SCUGen instance"
+  "Create a new SCUGen instance. Throws an error if any of the args are nil."
   [id name rate rate-name special args n-outputs]
-  (SCUGen. id name rate rate-name special args n-outputs))
+  (if (or (nil? id)
+          (nil? name)
+          (nil? rate)
+          (nil? rate-name)
+          (nil? special)
+          (nil? args)
+          (nil? n-outputs))
+    (throw (IllegalArgumentException. (str "Attempted to create an SCUGen with nil args. Got " [id name rate rate-name special args n-outputs])))
+    (SCUGen. id name rate rate-name special args n-outputs)))
 
 (defn count-ugen-args
   "Count the number of ugens in the args of ug (and their args recursively)"
@@ -23,7 +32,7 @@
             args)))
 
 (defmethod print-method SCUGen [ug w]
-  (.write w (str "#<sc-ugen: " (:name ug) " with " (count-ugen-args ug) " internal sc-ugens>")))
+  (.write w (str "#<sc-ugen: " (overtone-ugen-name (:name ug)) (:rate-name ug) " [" (count-ugen-args ug) "]>")))
 
 (defrecord ControlProxy [name value rate rate-name])
 (derive ControlProxy ::sc-ugen)
@@ -31,18 +40,33 @@
 (defn control-proxy
   "Create a new control proxy with the specified name, value and rate. Rate
   defaults to :kr. Specifically handles :tr which is really a TrigControl
-  ugen at :kr."
+  ugen at :kr. Throws an error if any of the args are nil."
   ([name value] (control-proxy name value :kr))
-  ([name value rate]
-     (ControlProxy. name value (if (= :tr)
-                                 (:kr RATES)
-                                 (rate RATES)) rate)))
+  ([name value rate-name]
+     (let [rate (if (= :tr)
+                  (:kr RATES)
+                  (rate-name RATES))]
+       (if (or (nil? name)
+               (nil? value)
+               (nil? rate)
+               (nil? rate-name))
+         (throw (IllegalArgumentException. (str "Attempted to create a ControlProxy with nil args. Got " [name value rate rate-name])))
+         (ControlProxy. name value rate rate-name)))))
 
 (defrecord OutputProxy [ugen rate rate-name index])
 (derive OutputProxy ::sc-ugen)
 
-(defn output-proxy [ugen index]
-  (OutputProxy. ugen (:rate ugen) (REVERSE-RATES (:rate ugen)) index))
+(defn output-proxy
+  "Create a new output proxy. Throws an error if any of the args are nil."
+  [ugen index]
+  (let [rate (:rate ugen)
+        rate-name (REVERSE-RATES (:rate ugen))]
+    (if (or (nil? ugen)
+            (nil? rate)
+            (nil? rate-name)
+            (nil? index))
+      (throw (IllegalArgumentException. (str "Attempted to create an OutputProxy with nil args. Got " [ugen rate rate-name index])))
+      (OutputProxy. ugen rate rate-name index))))
 
 
 (defn control-proxy? [obj] (= ControlProxy (type obj)))
