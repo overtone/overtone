9a3fd9b48c1d39cdef710ff47c157c849f479ea8
Sam Aaron <samaaron@gmail.com>
2011-08-26 17:32:25 +0100

    Add another compander example

--- src/overtone/sc/examples/compander.clj
+++ src/overtone/sc/examples/compander.clj
@@ -59,4 +59,22 @@
          poll-y (poll trig sb-val \"slope-below\")
 
          mixed (x-fade2 orig-sound noise-gate mix-val)]
-       [mixed mixed])"))
+       [mixed mixed])")
+
+  (:data
+   "View data coming out of a compander"
+   "Here, we don't produce any audio, rather we are interested in observing the polled values of both the input value (driven by the mouse x coord) and the output of the compander. We use use the silence ugen to ensure there's no audio output.
+
+Start by modifying the below value, say to 3. Notice how the value is only affected when it is below the threshold i.e. above the threshold the out val should be the same as the in val. Below the threshold the out value should be less than the in. Try increasing the below value and notice how the effect is magnified. Once you get the hang of this, start playing around with othe params to get a feel for what the compander is doing."
+   rate :ar
+   [thresh {:default 0.5 :doc "Threshold. The threshold defines which slope to use - above or below depending on whether the incoming value is greater or less than it"}
+    below  {:default 1 :doc "Slope below. Increase to reduce vals below threshold"}
+    above  {:default 1 :doc "Slope above. Increase to increase vals above threshold"}
+    clamp  {:default 0.01 :doc "Clamp time. Increase to introduce delay of effect if new out value is greater than current out value."}
+    relax  {:default 0.1 :doc "Relax time. Increase to introduce delay of effect if new out value is less than current out value."}]
+   "
+   (let [val   (k2a (mouse-x 0 1))
+         comp  (compander val val thresh below above clamp relax)
+         pollr (poll (impulse 5) val \"input val: \")
+         pollr (poll (impulse 5) comp \"compander out: \")]
+     (silent))"))
