99f5609b6527f2976897edaa11cf769f5829b07c
Sam Aaron <samaaron@gmail.com>
2011-09-08 15:31:19 +0100

    update osc-clj dependency to 0.6.2 which has error handling for bogus timestamps

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [overtone/scsynth-jna "0.1.2-SNAPSHOT"]
                  [overtone/at-at "0.1.1"]
-                 [overtone/osc-clj "0.6.1"]
+                 [overtone/osc-clj "0.6.2"]
                  [overtone/byte-spec "0.2.0-SNAPSHOT"]
                  [overtone/midi-clj "0.2.0-SNAPSHOT"]
                  [org.clojars.overtone/vijual "0.2.1"]]
