4a61e99f810f9a2a835ca55f0f182f65c5df9257
Sam Aaron <samaaron@gmail.com>
2011-09-14 14:01:08 +0100

    remove vijual stuff - perhaps might add it back in the future

--- project.clj
+++ project.clj
@@ -11,7 +11,6 @@
                  [overtone/at-at "0.1.1"]
                  [overtone/osc-clj "0.6.2"]
                  [overtone/byte-spec "0.2.0-SNAPSHOT"]
-                 [overtone/midi-clj "0.2.0-SNAPSHOT"]
-                 [org.clojars.overtone/vijual "0.2.1"]]
+                 [overtone/midi-clj "0.2.0-SNAPSHOT"]]
   :dev-dependencies [[marginalia "0.2.0"]]
   :jvm-opts ["-Xms256m" "-Xmx1g" "-XX:+UseConcMarkSweepGC"])
