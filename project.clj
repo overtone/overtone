99ba5c2330c1de2a5e372c83a8dc03f86558f402
Sam Aaron <samaaron@gmail.com>
2011-09-25 16:41:56 +0200

    bump to 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject overtone "0.3.0"
+(defproject overtone "0.4.0"
   :description "An audio/musical experiment."
   :url "http://project-overtone.org"
   :autodoc {:load-except-list [#"/test/" #"/classes/" #"/devices/"]
