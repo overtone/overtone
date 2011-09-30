0fafbbb837c8c2ccb0f9f4395685ae14995a9354
Sam Aaron <samaaron@gmail.com>
2011-09-30 22:04:23 +0200

    add docstrings for ugen done-action constants

--- src/overtone/sc/ugens.clj
+++ src/overtone/sc/ugens.clj
@@ -5,38 +5,21 @@
   (:use [overtone.sc.machinery.ugen fn-gen]))
 
 ;; Done actions are typically executed when an envelope ends, or a sample ends
-;; 0	do nothing when the UGen is finished
-;; 1	pause the enclosing synth, but do not free it
-;; 2	free the enclosing synth
-;; 3	free both this synth and the preceding node
-;; 4	free both this synth and the following node
-;; 5	free this synth; if the preceding node is a group then do g_freeAll on it, else free it
-;; 6	free this synth; if the following node is a group then do g_freeAll on it, else free it
-;; 7	free this synth and all preceding nodes in this group
-;; 8	free this synth and all following nodes in this group
-;; 9	free this synth and pause the preceding node
-;; 10	free this synth and pause the following node
-;; 11	free this synth and if the preceding node is a group then do g_deepFree on it, else free it
-;; 12	free this synth and if the following node is a group then do g_deepFree on it, else free it
-;; 13	free this synth and all other nodes in this group (before and after)
-;; 14	free the enclosing group and all nodes within it (including this synth)
-
-;;Done Actions
-(def NO-ACTION 0)
-(def PAUSE 1)
-(def FREE 2)
-(def FREE-AND-BEFORE 3)
-(def FREE-AND-AFTER 4)
-(def FREE-AND-GROUP-BEFORE 5)
-(def FREE-AND-GROUP-AFTER 6)
-(def FREE-UPTO-THIS 7)
-(def FREE-FROM-THIS-ON 8)
-(def FREE-PAUSE-BEFORE 9)
-(def FREE-PAUSE-AFTER 10)
-(def FREE-AND-GROUP-BEFORE-DEEP 11)
-(def FREE-AND-GROUP-AFTER-DEEP 12)
-(def FREE-CHILDREN 13)
-(def FREE-GROUP 14)
+(def NO-ACTION "Do nothing when the ugen is finished" 0)
+(def PAUSE "Pause the enclosing synth, but do not free it" 1)
+(def FREE "Free the enclosing synth" 2)
+(def FREE-AND-BEFORE "Free both this synth and the preceding node" 3)
+(def FREE-AND-AFTER "Free both this synth and the following node" 4)
+(def FREE-AND-GROUP-BEFORE "Free this synth; if the preceding node is a group then do g_freeAll on it, else free it" 5)
+(def FREE-AND-GROUP-AFTER "Free this synth; if the following node is a group then do g_freeAll on it, else free it" 6)
+(def FREE-UPTO-THIS "Free this synth and all preceding nodes in this group" 7)
+(def FREE-FROM-THIS-ON "Free this synth and all following nodes in this group" 8)
+(def FREE-PAUSE-BEFORE "Free this synth and pause the preceding node" 9)
+(def FREE-PAUSE-AFTER "Free this synth and pause the following node" 10)
+(def FREE-AND-GROUP-BEFORE-DEEP "Free this synth and if the preceding node is a group then do g_deepFree on it, else free it" 11)
+(def FREE-AND-GROUP-AFTER-DEEP "Free this synth and if the following node is a group then do g_deepFree on it, else free it" 12)
+(def FREE-CHILDREN "Free this synth and all other nodes in this group (before and after)" 13)
+(def FREE-GROUP "Free the enclosing group and all nodes within it (including this synth)" 14)
 
 ;;FFT Windows
 (def SINE 0)
