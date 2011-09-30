eea0106529d9cdb3528b4466c63e8ce94246553c
Sam Aaron <samaaron@gmail.com>
2011-09-30 22:19:06 +0200

    fix thx example that was only passing one arg to a binary op ugen

--- examples/thx.clj
+++ examples/thx.clj
@@ -14,11 +14,11 @@
 ;;}.play
 
 
-(demo 20
+(demo 25
       (let [target-pitches (map midi->hz [77 74 72 70 65 62 60 58 53 50 46 34])
             r-freq         (env-gen:kr (envelope [1 1 0.007] [8 6] [0 -4]) 1)
             amp-env        (env-gen:kr (envelope [0.07 0.07 0.21] [8 6] [0 1]) 1)
-            saws           (apply + (map #(+ (pan2 (saw (+ (* r-freq (+ 230 (* 100 (lf-noise2:kr 1.3))))
+            saws           (apply + (map #(pan2 (saw (+ (* r-freq (+ 230 (* 100 (lf-noise2:kr 1.3))))
                                                            (env-gen:kr (envelope [0 0 %] [8 6] [0 -3]) 1)))
-                                                   (lf-noise2:kr 1.3))) target-pitches))]
+                                                   (lf-noise2:kr 1.3)) target-pitches))]
         (* saws amp-env)))
