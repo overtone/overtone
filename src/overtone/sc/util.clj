ae3c55cd316adc7c092cba290271c365befe8d13
Sam Aaron <samaaron@gmail.com>
2011-10-02 14:09:33 +0100

    update map-to-ids to allow single vals as args, also rename to id-mapper

--- src/overtone/sc/util.clj
+++ src/overtone/sc/util.clj
@@ -3,14 +3,16 @@
       :author "Sam Aaron"}
     overtone.sc.util)
 
-(defn map-to-ids
+(defn id-mapper
   "Map all elements of col which are associative and have an :id key to the
   associated :id value. Useful for extracting ids from arglists containing
-  buffers and busses."
+  buffers and busses. Also works if col is not a collection but a single
+  value."
   [col]
-  (map (fn [el]
-         (if (and (associative? el)
-                  (:id el))
-           (:id el)
-           el))
-       col))
+  (let [map-fn (fn [el] (if (and (associative? el)
+                                (:id el))
+                         (:id el)
+                         el))]
+    (if (sequential? col)
+      (map map-fn col)
+      (map-fn col))))
