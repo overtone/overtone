cc19f071cda88bdd71edaa2c6fdf532396708554
Sam Aaron <samaaron@gmail.com>
2011-10-06 17:05:18 +0100

    add docstrings for default vars

--- src/overtone/sc/machinery/defaults.clj
+++ src/overtone/sc/machinery/defaults.clj
@@ -1,44 +1,70 @@
 (ns overtone.sc.machinery.defaults
   (:require [overtone.at-at :as at-at]))
 
-;; ## SCSynth limits
-(def MAX-NODES 4000)
-(def MAX-BUFFERS 4000)
-(def MAX-SDEFS 2000)
-(def MAX-AUDIO-BUS 1000)
-(def MAX-CONTROL-BUS 4000)
+(def MAX-NODES
+  "Maximum number of executing nodes allowed in the server"
+  4000)
 
-;;Number of audio busses to reserve. These busses won't be available to users
-;;via overtone.sc.bus/audio-bus
-(def AUDIO-BUS-RESERVE-COUNT 50)
+(def MAX-BUFFERS
+  "Maximum number of audio buffers allowed in the server"
+  4000)
 
+(def MAX-SDEFS
+  "Maximum number of SynthDefs allowed in the server"
+  2000)
 
-(def SERVER-HOST "127.0.0.1")
-(def SERVER-PORT nil) ; nil means a random port
-(def N-RETRIES 20)
+(def MAX-AUDIO-BUS
+  "Maximum number of audio busses allowed in the server"
+  1000)
 
-;; Max number of milliseconds to wait for a reply from the server
-(def REPLY-TIMEOUT 500)
+(def MAX-CONTROL-BUS
+  "Maximum number of control busses allowed in the server"
+  4000)
 
-;;Max number of samples supported in a UDP OSC message
-(def MAX-OSC-SAMPLES 8192)
+(def AUDIO-BUS-RESERVE-COUNT
+  "Number of audio busses to reserve. These busses won't be available to users
+  via overtone.sc.bus/audio-bus"
+  50)
 
-;id of root group in node tree on server
-(def ROOT-GROUP 0)
+(def SERVER-PORT
+  "Default port number used when booting external server. If nil, a random port is used"
+  nil)
 
-;;make an at-at pool for all default scheduling
-(def SC-POOL (at-at/mk-pool))
+(def N-RETRIES
+  "Number of times to attempt to connect to an externally booted server"
+  50)
 
-(def SC-PATHS {:linux ["scsynth"]
-               :windows ["C:/Program Files/SuperCollider/scsynth.exe"
-                         "D:/Program Files/SuperCollider/scsynth.exe"
-                         "E:/Program Files/SuperCollider/scsynth.exe"
-                         "C:/Program Files (x86)/SuperCollider/scsynth.exe"
-                         "D:/Program Files (x86)/SuperCollider/scsynth.exe"
-                         "E:/Program Files (x86)/SuperCollider/scsynth.exe"]
+(def REPLY-TIMEOUT
+  "Max number of milliseconds to wait for a reply from the server"
+  500)
 
-               :mac  ["/Applications/SuperCollider/scsynth"] })
+(def MAX-OSC-SAMPLES
+  "Max number of samples supported in a UDP OSC message"
+  8192)
 
-(def SC-ARGS  {:linux []
-               :windows []
-               :mac   ["-U" "/Applications/SuperCollider/plugins"] })
+(def ROOT-GROUP
+  "ID of root group in node tree on server"
+  0)
+
+(def SC-POOL
+  "make an at-at pool for all default scheduling"
+  (at-at/mk-pool))
+
+(def SC-PATHS
+  "Default system paths to an externally installed SuperCollider server for
+  various operating systems."
+  {:linux ["scsynth"]
+   :windows ["C:/Program Files/SuperCollider/scsynth.exe"
+             "D:/Program Files/SuperCollider/scsynth.exe"
+             "E:/Program Files/SuperCollider/scsynth.exe"
+             "C:/Program Files (x86)/SuperCollider/scsynth.exe"
+             "D:/Program Files (x86)/SuperCollider/scsynth.exe"
+             "E:/Program Files (x86)/SuperCollider/scsynth.exe"]
+   :mac  ["/Applications/SuperCollider/scsynth"] })
+
+(def SC-ARGS
+  "Extra arguments required to correctly boot an external SuperCollider server
+  for various operating systems."
+  {:linux []
+   :windows []
+   :mac   ["-U" "/Applications/SuperCollider/plugins"]})
