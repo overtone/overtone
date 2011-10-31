38e96dae44b82843820e8545e1afb9589a6fd1b0
Sam Aaron <samaaron@gmail.com>
2011-10-31 21:25:06 +0000

    add defcgen to gens ns to give users access to defcgen

--- src/overtone/sc/gens.clj
+++ src/overtone/sc/gens.clj
@@ -4,10 +4,11 @@
   overtone.sc.gens
   (:use [overtone.util.ns :only [immigrate]])
   (:require [overtone.sc.machinery.cgens oscillators demand mix io]
-            [overtone.sc.machinery ugens]))
+            [overtone.sc.machinery ugens defcgen]))
 
 (immigrate
  'overtone.sc.machinery.ugens
+ 'overtone.sc.machinery.defcgen
  'overtone.sc.machinery.cgens.oscillators
  'overtone.sc.machinery.cgens.demand
  'overtone.sc.machinery.cgens.mix
