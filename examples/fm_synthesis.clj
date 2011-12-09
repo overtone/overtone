6361cad2ee93ce1afd6192aa8b715b661cbdf622
Jeff Rose <rosejn@gmail.com>
2011-12-09 10:51:29 +0700

    Now with improved fm synthesis demo.
    * Thanks to Harold for posting this to the Overtone list, now
    we have some decent fm synth sounds

--- examples/fm_synthesis.clj
+++ examples/fm_synthesis.clj
@@ -1,26 +1,38 @@
 (ns examples.fm-synthesis
   (:use overtone.live))
 
-(refer-ugens)
-
-(defsynth fm [carrier 440 modulator 440 depth 0.1]
-  (let [mod-env (env-gen (perc 0.3 0.6))
-        amp-env (env-gen (perc 0.01 0.8) 1 1 0 1 FREE)]
+(definst fm [carrier 440 divisor 2.0 depth 1.0]
+  (let [modulator (/ carrier divisor)
+        mod-env (env-gen (lin-env 1 0 1))
+        amp-env (env-gen (lin-env 0 1 1))]
     (* amp-env
        (sin-osc (+ carrier
-                 (* depth mod-env
-                    (sin-osc modulator)))))))
+                   (* mod-env  (* carrier depth) (sin-osc modulator)))))))
 
-(fm 440 660 880)
-(reset)
-
-; Play this one, and then try moving your mouse around and clicking
-(defsynth fm-synth [gate 1]
-  (let [modulator (+ 0.1 (* (mouse-button:kr 0 1) (sin-osc (mouse-x:kr 20 4000))))
-        carrier (sin-osc (* (mouse-y:kr 4000, 0) modulator))]
-    (out 0 (pan2 carrier))))
-
-(fm-synth)
-(fm-synth :ctl :gate 0)
-
-(fm-synth 220)
+; Some of these are more or less interesting
+(fm)
+(fm 220)
+(fm 220 3)
+(fm 220 10)
+(fm 440)
+(fm 440 2 4)
+(fm 440 2 8)
+(fm 440 4 4)
+(fm 220 4 8)
+(fm 880 4 4)
+(fm 110 4 4)
+(fm 220 2 4)
+(fm 220 2 8)
+(fm 440 8 8)
+(fm 440 8 2)
+(fm 440 (/ 4 3) 2)
+(fm 440 (/ 5 3) 2)
+(fm 440 (/ 7 3) 2)
+(fm 440 (/ 4 3) 4)
+(fm 440 (/ 5 3) 4)
+(fm 440 (/ 7 3) 4)
+(fm 220 (/ 7 5) 2)
+(fm 220 (/ 7 5) 4)
+(fm 110 4 2)
+(fm 110 4 4)
+(fm 110 4 8)
