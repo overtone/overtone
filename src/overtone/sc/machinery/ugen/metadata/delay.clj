cff7594e15997e64839ae08309eba51552510b57
Sam Aaron <samaaron@gmail.com>
2011-12-17 18:24:39 +0000

    fix delay ugen arg names

--- src/overtone/sc/machinery/ugen/metadata/delay.clj
+++ src/overtone/sc/machinery/ugen/metadata/delay.clj
@@ -5,12 +5,12 @@
 
   [
    ;; Delay1 : UGen {
-   ;; 	*ar { arg in = 0.0, mul = 1.0, add = 0.0;
-   ;; 		^this.multiNew('audio', in).madd(mul, add)
-   ;; 	}
-   ;; 	*kr { arg in = 0.0, mul = 1.0, add = 0.0;
-   ;; 		^this.multiNew('control', in).madd(mul, add)
-   ;; 	}
+   ;;   *ar { arg in = 0.0, mul = 1.0, add = 0.0;
+   ;;           ^this.multiNew('audio', in).madd(mul, add)
+   ;;   }
+   ;;   *kr { arg in = 0.0, mul = 1.0, add = 0.0;
+   ;;           ^this.multiNew('control', in).madd(mul, add)
+   ;;   }
    ;; }
 
    {:name "Delay1",
@@ -25,18 +25,18 @@
    ;; these delays use real time allocated memory.
 
    ;; DelayN : UGen {
-   ;; 	*ar { arg in = 0.0, maxdelaytime = 0.2, delaytime = 0.2, mul = 1.0, add = 0.0;
-   ;; 		^this.multiNew('audio', in.asAudioRateInput, maxdelaytime, delaytime).madd(mul, add)
-   ;; 	}
-   ;; 	*kr { arg in = 0.0, maxdelaytime = 0.2, delaytime = 0.2, mul = 1.0, add = 0.0;
-   ;; 		^this.multiNew('control', in, maxdelaytime, delaytime).madd(mul, add)
-   ;; 	}
+   ;;   *ar { arg in = 0.0, maxdelaytime = 0.2, delaytime = 0.2, mul = 1.0, add = 0.0;
+   ;;           ^this.multiNew('audio', in.asAudioRateInput, maxdelaytime, delaytime).madd(mul, add)
+   ;;   }
+   ;;   *kr { arg in = 0.0, maxdelaytime = 0.2, delaytime = 0.2, mul = 1.0, add = 0.0;
+   ;;           ^this.multiNew('control', in, maxdelaytime, delaytime).madd(mul, add)
+   ;;   }
    ;; }
 
    {:name "DelayN",
     :args [{:name "in", :default 0.0, :mode :as-ar :doc "the input signal"}
-           {:name "maxdelaytime", :default 0.2 :doc "the maximum delay time in seconds. used to initialize the delay buffer size"}
-           {:name "delaytime", :default 0.2 :doc "delay time in seconds"}]
+           {:name "max-delay-time", :default 0.2 :doc "the maximum delay time in seconds. Used to initialize the delay buffer size"}
+           {:name "delay-time", :default 0.2 :doc "delay time in seconds"}]
     :doc "simple delay line, no interpolation. See also DelayL which uses linear interpolation, and DelayC which uses cubic interpolation. Cubic interpolation is more computationally expensive than linear, but more accurate."}
 
    ;; DelayL : DelayN {}
@@ -60,9 +60,9 @@
 
    {:name "CombN",
     :args [{:name "in", :default 0.0, :mode :as-ar :doc "the input signal"}
-           {:name "maxdelaytime", :default 0.2 :doc "the maximum delay time in seconds. used to initialize the delay buffer size"}
-           {:name "delaytime", :default 0.2 :doc "delay time in seconds"}
-           {:name "decaytime", :default 1.0 :doc "time for the echoes to decay by 60 decibels. If this time is negative then the feedback coefficient will be negative, thus emphasizing only odd harmonics at an octave lower."}]
+           {:name "max-delay-time", :default 0.2 :doc "the maximum delay time in seconds. Used to initialize the delay buffer size"}
+           {:name "delay-time", :default 0.2 :doc "delay time in seconds"}
+           {:name "decay-time", :default 1.0 :doc "time for the echoes to decay by 60 decibels. If this time is negative then the feedback coefficient will be negative, thus emphasizing only odd harmonics at an octave lower."}]
     :doc "comb delay line, no interpolation. See also CombL which uses linear interpolation, and CombC which uses cubic interpolation. Cubic interpolation is more computationally expensive than linear, but more accurate."}
 
    {:name "CombL" :extends "CombN"
@@ -83,18 +83,18 @@
    ;; these delays use shared buffers.
 
    ;; BufDelayN : UGen {
-   ;; 	*ar { arg buf = 0, in = 0.0, delaytime = 0.2, mul = 1.0, add = 0.0;
-   ;; 		^this.multiNew('audio', buf, in.asAudioRateInput, delaytime).madd(mul, add)
-   ;; 	}
-   ;; 	*kr { arg buf = 0, in = 0.0, delaytime = 0.2, mul = 1.0, add = 0.0;
-   ;; 		^this.multiNew('control', buf, in, delaytime).madd(mul, add)
-   ;; 	}
+   ;;   *ar { arg buf = 0, in = 0.0, delaytime = 0.2, mul = 1.0, add = 0.0;
+   ;;           ^this.multiNew('audio', buf, in.asAudioRateInput, delaytime).madd(mul, add)
+   ;;   }
+   ;;   *kr { arg buf = 0, in = 0.0, delaytime = 0.2, mul = 1.0, add = 0.0;
+   ;;           ^this.multiNew('control', buf, in, delaytime).madd(mul, add)
+   ;;   }
    ;; }
 
    {:name "BufDelayN",
     :args [{:name "buf", :default 0.0 :doc "buffer number"}
            {:name "in", :default 0.0 :mode :as-ar :doc "the input signal"}
-           {:name "delaytime", :default 0.2 :doc "delay time in seconds"}]
+           {:name "delay-time", :default 0.2 :doc "delay time in seconds"}]
     :doc "buffer based simple delay line with no interpolation. See also BufDelayL which uses linear interpolation, and BufDelayC which uses cubic interpolation. Cubic interpolation is more computationally expensive than linear, but more accurate."}
 
    ;; BufDelayL : BufDelayN {}
@@ -108,16 +108,16 @@
     :doc "buffer based simple delay line with cubic interpolation"}
 
    ;; BufCombN : UGen {
-   ;; 	*ar { arg buf = 0, in = 0.0, delaytime = 0.2, decaytime = 1.0, mul = 1.0, add = 0.0;
-   ;; 		^this.multiNew('audio', buf, in.asAudioRateInput, delaytime, decaytime).madd(mul, add)
-   ;; 	}
+   ;;   *ar { arg buf = 0, in = 0.0, delaytime = 0.2, decaytime = 1.0, mul = 1.0, add = 0.0;
+   ;;           ^this.multiNew('audio', buf, in.asAudioRateInput, delaytime, decaytime).madd(mul, add)
+   ;;   }
    ;; }
 
    {:name "BufCombN",
     :args [{:name "buf", :default 0 :doc "buffer number"}
            {:name "in", :default 0.0, :mode :as-ar :doc "the input signal"}
-           {:name "delaytime", :default 0.2 :doc "delay time in seconds"}
-           {:name "decaytime", :default 1.0 :doc "time for the echoes to decay by 60 decibels. If this time is negative then the feedback coefficient will be negative, thus emphasizing only odd harmonics at an octave lower."}],
+           {:name "delay-time", :default 0.2 :doc "delay time in seconds"}
+           {:name "decay-time", :default 1.0 :doc "time for the echoes to decay by 60 decibels. If this time is negative then the feedback coefficient will be negative, thus emphasizing only odd harmonics at an octave lower."}],
     :rates #{:ar}
     :doc "buffer based comb delay line with no interpolation. See also [BufCombL] which uses linear interpolation, and BufCombC which uses cubic interpolation. Cubic interpolation is more computationally expensive than linear, but more accurate."}
 
