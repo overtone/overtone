dca25bcd4c7a5ae3f0eed5cff907dc2762bf92d2
Jacob Lee <artdent@gmail.com>
2012-01-01 15:07:13 -0500

    Fix some unit tests.

--- test/overtone/deps_test.clj
+++ test/overtone/deps_test.clj
@@ -1,28 +1,25 @@
 (ns overtone.deps-test
   (:use clojure.test
-        overtone.deps))
+        overtone.libs.deps))
 
 (deftest deps-basic-test
   (reset-deps)
   (let [log (atom [])]
     (on-deps :foo ::swapper
-            #(swap! log conj :a))
+             #(swap! log conj :a))
     (satisfy-deps :foo)
     (on-deps :foo ::another-swapper
-            #(swap! log conj :b))
+             #(swap! log conj :b))
     (on-deps [:foo :bar] ::third-swapper
-            #(swap! log conj :c))
+             #(swap! log conj :c))
     (on-deps #{:foo :bar :baz} ::fourth-swapper
-            #(swap! log conj :d))
+             #(swap! log conj :d))
     (satisfy-deps :bar)
     (Thread/sleep 100)
     (satisfy-deps :baz)
     (Thread/sleep 100)
-    (with-deps #{:foo :baz}
-               #(swap! log conj :e))
-    (Thread/sleep 100)
     (on-deps #{:foo :baz} ::fifth-swapper
-            #(swap! log conj :e))
+             #(swap! log conj :e))
     (Thread/sleep 300)
     (is (= [:a :b :c :d :e] @log))))
 
