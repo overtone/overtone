5aa401d9ee63b94ee680047359676944da11e621
Dave Ray <daveray@gmail.com>
2012-01-11 21:39:34 -0500

    When synth control ui is closed, clean up bindings to the synth's value atom. Otherwise, invisible controls can pile up in the background. This changes requires latest from seesaw 1.3.1-SNAPSHOT develop branch.

--- src/overtone/gui/control.clj
+++ src/overtone/gui/control.clj
@@ -21,49 +21,30 @@
         items  [[label "width 80:80:100"]
                 [spinner "width 80:80:80"]
                 [slider "wrap"]]]
-    (bind/bind slider
-               (bind/transform (fn [v]
-                                 (let [val (+ min-val (* (- max-val min-val)
-                                                         (/ v (double SLIDER-MAX))))]
-                                   val)))
-               val-atom)
-    (bind/bind val-atom
-               (bind/transform #(* (/ (- % min-val) (- max-val min-val)) SLIDER-MAX))
-               ; if the value's "close enough" to the spinner, stop. Otherwise,
-               ; we get infinite loops caused by rounding error.
-               (bind/filter #(>= (Math/abs (- % (value slider))) 0.01))
-               slider)
-    (bind/bind spinner val-atom)
-    (bind/bind val-atom 
-               ; if the value's "close enough" to the slider, stop. Otherwise,
-               ; we get infinite loops caused by rounding error.
-               (bind/filter #(>= (Math/abs (- % (value spinner))) 0.01))
-               spinner)
-    items))
 
-(defn- control-dial
-  [name init-val min-val max-val step val-atom]
-  (let [spin-model (spinner-model (double init-val)
-                                  :from (double min-val) :to (double max-val) :by (double step))
-        spinner (spinner :model spin-model)
-        dial (dial :id (keyword name) :class :synth-control-dial
-                        :value init-val :min 0 :max SLIDER-MAX :orientation :horizontal)
-        label  (label name)
-        items  [[label "width 80:80:100"]
-                [spinner "width 80:80:80"]
-                [dial "wrap"]]]
-    (bind/bind dial
-               (comment bind/transform (fn [v]
-                                 (let [val (+ min-val (* (- max-val min-val)
-                                                         (/ v (double SLIDER-MAX))))]
-                                   val)))
-               val-atom)
-    (bind/bind val-atom
-               ;(bind/transform #(* (/ (- % min-val) (- max-val min-val)) SLIDER-MAX))
-               dial)
-    (bind/bind spinner val-atom)
-    (bind/bind val-atom spinner)
-    items))
+    ; return 2 element vector of items and cleanup function. hmmm.
+    [items
+     (juxt 
+       (bind/bind 
+         slider
+         (bind/transform (fn [v]
+                           (let [val (+ min-val (* (- max-val min-val)
+                                                   (/ v (double SLIDER-MAX))))]
+                             val)))
+         val-atom
+         (bind/transform #(* (/ (- % min-val) (- max-val min-val)) SLIDER-MAX))
+         ; if the value's "close enough" to the spinner, stop. Otherwise,
+         ; we get infinite loops caused by rounding error.
+         (bind/filter #(>= (Math/abs (- % (value slider))) 0.01))
+         slider)
+
+       (bind/bind 
+         spinner  
+         val-atom 
+         ; if the value's "close enough" to the slider, stop. Otherwise,
+         ; we get infinite loops caused by rounding error.
+         (bind/filter #(>= (Math/abs (- % (value spinner))) 0.01))
+         spinner))]))
 
 (defn synth-controller
   "Create a GUI for the given synth or instrument.  Requires sufficient parameter metadata in the synthdef.
@@ -84,16 +65,39 @@
                                         (nil? (:step %))))
                               (:params synth))]
       (if (zero? (count full-params))
-        (alert (str "The " (:name synth) " synth or instrument does not have any parameters
-with the required meta-data.  (e.g.  [freq {:default 80 :min 40 :max 880 :step 1}])"))
-        (let [control-panes (mapcat
+        (alert (str "The '" (:name synth) "'"
+                    " synth or instrument does not have any parameters"
+                    " with the required meta-data. "
+                    " (e.g.  [freq {:default 80 :min 40 :max 880 :step 1}])"))
+        (let [control-panes (map
                               (fn [{:keys [name default min max step value]}]
                                 (control-slider name default min max step value))
                               full-params)
+              cleanup       (apply juxt (map second control-panes))
               pane (mig-panel :constraints ["" "[right][center][center]" ""]
-                              :items control-panes)
+                              :items (mapcat first control-panes))
               frame (frame :title (:name synth)
                            :content pane
                            :on-close :dispose)]
+          ; When the window is closed, unhook everything from the synth
+          ; so the ui can be garbage collected, etc.
+          (listen frame :window-closed (fn [_] (cleanup)))
           (-> frame pack! show!))))))
 
+(comment
+  (use 'overtone.gui.control)
+  (def synth {:name "Test"
+              :type :test-synth
+              :params [{:name "freq"
+                       :default 440
+                       :min 40
+                       :max 880
+                       :step 1
+                       :value (atom 440) }
+                       {:name "amp"
+                       :default 5.0 
+                       :min 0.0 
+                       :max 10.0 
+                       :step 0.1
+                       :value (atom 2.5) }]})
+  (synth-controller synth))
