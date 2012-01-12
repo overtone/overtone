81ea38511674875aa08a5f50994abcea81e0f126
Dave Ray <daveray@gmail.com>
2012-01-11 22:31:24 -0500

    synth-controller can now display controls for multiple synths.

--- src/overtone/gui/control.clj
+++ src/overtone/gui/control.clj
@@ -56,43 +56,58 @@
          (bind/filter #(>= (Math/abs (- % (value spinner))) 0.01))
          spinner))]))
 
-(defn synth-controller
-  "Create a GUI for the given synth or instrument.  Requires sufficient parameter metadata in the synthdef.
-
-  (defsynth foo [freq {:default 440 :min 20 :max 10000 :step 1}]
-    (out 0 (* [0.6 0.6] (env-gen (perc 0.01 0.2) :action FREE) (sin-osc freq))))
-
-  (gui-for foo) ; pops up gui window that will modify active synths and default param values
-
-  ; To reset the synth back to the original default paremeter values use
-  (reset-synth-defaults foo)
-  "
+(defn synth-controller-panel
   [synth]
-  (invoke-now
-    (let [full-params (filter #(not (or (= :none (:type %))
+  (let [full-params (filter #(not (or (= :none (:type %))
                                         (nil? (:min %))
                                         (nil? (:max %))
                                         (nil? (:step %))))
                               (:params synth))]
       (if (zero? (count full-params))
-        (alert (str "The '" (:name synth) "'"
+        (do
+          (alert (str "The '" (:name synth) "'"
                     " synth or instrument does not have any parameters"
                     " with the required meta-data. "
                     " (e.g.  [freq {:default 80 :min 40 :max 880 :step 1}])"))
+          {:synth synth
+           :panel (label :text "Missing parameters" :border (:name synth))
+           :cleanup (fn [])})
         (let [control-panes (map
                               (fn [{:keys [name default min max step value]}]
                                 (control-slider name min max step value))
                               full-params)
               cleanup       (apply juxt (map second control-panes))
-              pane (mig-panel :constraints ["" "[right][center][center]" ""]
-                              :items (mapcat first control-panes))
-              frame (frame :title (:name synth)
-                           :content pane
-                           :on-close :dispose)]
-          ; When the window is closed, unhook everything from the synth
-          ; so the ui can be garbage collected, etc.
-          (listen frame :window-closed (fn [_] (cleanup)))
-          (-> frame pack! show!))))))
+              panel (mig-panel :constraints ["" "[right][center][center]" ""]
+                               :items (mapcat first control-panes)
+                               :border (:name synth))]
+          {:synth synth 
+           :panel panel 
+           :cleanup cleanup}))))
+
+(defn synth-controller
+  "Create a GUI for the given synths or instruments.  Each synth must have sufficient 
+   parameter metadata in the synthdef.
+
+    (defsynth foo [freq {:default 440 :min 20 :max 10000 :step 1}]
+      (out 0 (* [0.6 0.6] (env-gen (perc 0.01 0.2) :action FREE) (sin-osc freq))))
+    
+    ; pops up gui window that will modify active synths and default param values 
+    (synth-controller foo) 
+
+    ; To reset the synth back to the original default paremeter values use
+    (reset-synth-defaults foo)
+  "
+  [& synths]
+  (invoke-now
+    (let [panels  (map synth-controller-panel synths)
+          cleanup (apply juxt (map :cleanup panels))
+          frame (frame :title "Synth Controllers"
+                       :content (vertical-panel :items (map :panel panels))
+                       :on-close :dispose)]
+      ; When the window is closed, unhook everything from the synth
+      ; so the ui can be garbage collected, etc.
+      (listen frame :window-closed (fn [_] (cleanup)))
+      (-> frame pack! show!))))
 
 (comment
   (use 'overtone.gui.control)
