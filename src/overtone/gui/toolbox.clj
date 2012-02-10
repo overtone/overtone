bb19c908ebf2c37358c930a45767797b628400ed
Kevin Neaton <neatonk@gmail.com>
2012-02-10 08:18:19 -0600

    Work on toolbox...
    Use custom-dialog instead of JWindow.
    
    * The dialog is modal and returns the new selection.
    * The dialog is created in popup-toolbox instead of toolbox
      since it is good for one use only.
    
    Use protocols and datatypes
    
    * Added Toolbox datatype.
    * Extend ToWidget, Selection, MakeWidget, and Value protocols.
    * Use Toolbox in place of previous swing widget.
    * Use Toolbox to store tool selection, no need for button-group.
    
    TODO:
    * add a real example.
    * change the cursor to show which tool is active.
    * use custom icons.

--- src/overtone/gui/toolbox.clj
+++ src/overtone/gui/toolbox.clj
@@ -1,51 +1,88 @@
 (ns overtone.gui.toolbox
-  (:use [seesaw.core])
+  (:use [seesaw core
+         [selection :only [Selection]]
+         [make-widget :only [MakeWidget]]
+         [to-widget :only [ToWidget]]
+         [value :only [Value]]])
   (:import javax.swing.JWindow
            java.awt.MouseInfo))
 
+(defrecord Toolbox [tools primary secondary])
+
+(extend-type Toolbox
+
+  ToWidget
+  (to-widget* [tbox] tbox)
+
+  Selection
+  (get-selection [tbox]
+    (list @(:primary tbox)))
+  (set-selection [tbox [v]]
+    (reset! (:primary tbox) v))
+
+  MakeWidget
+  (make-widget* [tbox]
+    (invoke-now
+     (flow-panel :align :left :items (:tools tbox))))
+
+  Value
+  (container?* [tbox] false)
+  (value* [tbox]
+    (let [{:keys [primary secondary]} tbox]
+      {:primary @primary :secondary @secondary}))
+  (value!* [tbox v]
+    (let [{:keys [primary secondary]} v]
+      (reset! (:primary tbox) primary)
+      (reset! (:secondary tbox) secondary)
+      (value tbox))))
+
 (defn tool
-  "Returns a new tool (a tqoggle-button for now) for the given tool-spec."
+  "Returns a new tool (a button for now) for the given tool-spec."
   [& tool-spec]
   (let [tool-spec (if (= (count tool-spec) 1)
                     (first tool-spec)
                     tool-spec)
         tool-spec ((comp flatten seq) tool-spec)]
-
-  (apply toggle tool-spec)))
+    (apply button tool-spec)))
 
 (def ^:private pointer-spec
   {:id :pointer
    :text "Pointer"
+   :cursor :default
    :selected? true})
 
 (def ^:private hand-spec
   {:id :hand
-   :text "Hand"})
+   :text "Hand"
+   :cursor :hand})
 
-(defn toolbox-bg
-  [& tool-specs]
-  (let [tool-specs (or (not-empty tool-specs)
-                       [pointer-spec hand-spec])
-        tools      (map tool tool-specs)]
+(defn default-tools
+  []
+  (map tool [pointer-spec hand-spec]))
 
-    (button-group :buttons tools)))
+(defn- add-behavior
+  ([tbox]
+     (let [tools (:tools tbox)]
+       (listen tools :mouse-clicked
+               (fn [e]
+                 (let [id (id-of e)]
+                   (selection! tbox [(to-widget e)])
+                   (return-from-dialog e (id-of e)))))
+       tbox)))
 
 (defn toolbox
-  "Create a temporary popup window containing a set of selectable tools."
+  "Create a Toolbox instance with a set of 'clickable' tools and initial primary
+   and secondary tool selections."
   ([]
-     (toolbox (toolbox-bg)))
-  ([tools-bg]
-     (invoke-now
-      (let [popup (JWindow.)
-            tools (enumeration-seq (.getElements tools-bg))
-            body (flow-panel :align :left :items tools)]
+     (toolbox (default-tools)))
+  ([tools]
+     (let [primary   (or (first (filter :selected? tools))
+                         (first tools))
+           secondary (or (second (filter :selected? tools))
+                         primary)]
 
-        (listen tools-bg :mouse-clicked (fn [e] (hide! popup)))
-
-        (doto popup
-          (.add body)
-          (.pack))
-        popup))))
+       (add-behavior
+        (Toolbox. tools (atom primary) (atom secondary))))))
 
 (def ^:dynamic *toolbox* (toolbox))
 
@@ -57,8 +94,7 @@
   ([]
      (current-tool *toolbox*))
   ([tbox]
-     ((comp key first) (filter (comp true? val)
-                               (value tbox)))))
+     (id-of (selection tbox))))
 
 (defn popup-toolbox
   "Show the toolbox until clicked."
@@ -67,17 +103,16 @@
   ([tbox]
      (popup-toolbox tbox :to (mouse-location)))
   ([tbox & opts]
-     (apply move! tbox opts)
-     (show! tbox)))
+     (let [popup (custom-dialog :modal? true :content tbox)]
+       (apply move! popup opts)
+       (-> popup
+           pack!
+           show!))))
 
 (comment
-
   ;; global toolbox
   (popup-toolbox)
   (current-tool)
-
-  ;;local toolbox
-  (toolbox (toolbox-bg))
 
   ;; thread-local toolbox.
   (binding [*toolbox* (toolbox)]
