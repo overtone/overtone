a14e9fda8259f2bb616a7cd38516ba1cf78ff0f6
Sam Aaron <samaaron@gmail.com>
2012-03-13 10:35:23 -0700

    Upgrade to at-at 1.0.0-SNAPSHOT - now with runtime schedule info. Try print-schedule :-)

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/data.json "0.1.2"]
                  [overtone/scsynth-jna "0.1.2-SNAPSHOT"]
-                 [overtone/at-at "0.2.1"]
+                 [overtone/at-at "1.0.0-SNAPSHOT"]
                  [overtone/osc-clj "0.7.1"]
                  [overtone/byte-spec "0.3.1"]
                  [overtone/midi-clj "0.2.1"]
