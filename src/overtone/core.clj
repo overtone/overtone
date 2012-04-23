a3be21f72de390797630f956a522c8c08e27a416
Sam Aaron <samaaron@gmail.com>
2012-04-23 21:53:08 +0100

    update to match overtone.live

--- src/overtone/core.clj
+++ src/overtone/core.clj
@@ -1,15 +1,13 @@
 (ns overtone.core
   (:use [overtone.util ns])
   (:require clojure.stacktrace
-            [overtone.util lib]
             [overtone.config store]
             [overtone.midi]
             [overtone.osc]
             [overtone.algo chance scaling trig]
-            [overtone.sc bindings buffer bus envelope example ugens info
-                         mixer node sample server synth trigger]
+            [overtone.sc bindings buffer bus envelope example info
+                         ugens defcgen mixer node sample server synth trigger]
             [overtone.sc.cgens oscillators demand mix io buf-io env tap]
-            [overtone.sc ugens defcgen]
             [overtone.music rhythm pitch tuning time]
             [overtone.studio mixer inst util fx wavetable]
             [overtone.repl ugens examples shell inst]
@@ -19,7 +17,6 @@
             [overtone.gui control]))
 
 (immigrate
- 'overtone.util.lib
  'overtone.osc
  'overtone.midi
  'overtone.algo.chance
@@ -31,6 +28,13 @@
  'overtone.sc.bus
  'overtone.sc.envelope
  'overtone.sc.example
+ 'overtone.sc.info
+ 'overtone.sc.mixer
+ 'overtone.sc.node
+ 'overtone.sc.sample
+ 'overtone.sc.server
+ 'overtone.sc.synth
+ 'overtone.sc.trigger
  'overtone.sc.ugens
  'overtone.sc.defcgen
  'overtone.sc.cgens.oscillators
@@ -40,13 +44,6 @@
  'overtone.sc.cgens.buf-io
  'overtone.sc.cgens.env
  'overtone.sc.cgens.tap
- 'overtone.sc.info
- 'overtone.sc.mixer
- 'overtone.sc.node
- 'overtone.sc.sample
- 'overtone.sc.server
- 'overtone.sc.synth
- 'overtone.sc.trigger
  'overtone.music.rhythm
  'overtone.music.pitch
  'overtone.music.tuning
@@ -63,5 +60,4 @@
  'overtone.libs.event
  'overtone.libs.freesound
  'overtone.version
- 'overtone.viz.scope
- )
+ 'overtone.viz.scope)
