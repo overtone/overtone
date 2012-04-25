0fedc39f9918ad72497d228abea81f3a0083f598
Jeff Rose <rosejn@gmail.com>
2012-04-25 22:12:53 +0100

    Adding a bit more logging to dependencies.
    * this helps debug broken dependency handlers

--- src/overtone/libs/deps.clj
+++ src/overtone/libs/deps.clj
@@ -59,18 +59,21 @@
 
 (defn- satisfy*
   [{:keys [satisfied todo done]} new-deps]
-  ;(log/info (format "new-deps: %s" new-deps))
   (let [satisfied (set/union satisfied new-deps)
-        ;_ (log/info (format "satisfied: %s" satisfied))
         execute-tasks (fn [[final-done final-todo] [key deps task]]
-                        ;(log/info (format "Satisfied? = %s --- sat: %s deps: %s" (set/superset? satisfied deps) satisfied deps))
                         (if (set/superset? satisfied deps)
                           (do
-                            ;(log/info "Satisfying deps: " deps)
-                            (task)
+                            (log/info "Running dep handler: " key)
+                            (try
+                              (task)
+                              (catch Exception e
+                                (log/error (format "Exception in dependency handler: %s\n%s"
+                                                   key
+                                                   (with-out-str (.printStackTrace e))))))
                             [(conj final-done [key deps task]) final-todo])
                           [final-done (conj final-todo [key deps task])]))
         [t-done t-todo] (reduce execute-tasks [done []] todo)]
+    (log/info "deps-satisfied: " satisfied)
     {:satisfied satisfied
      :done t-done
      :todo t-todo}))
@@ -99,6 +102,7 @@
   "Specifies that a list of dependencies have been satisfied. Uses an agent so
   it's safe to call this from within a transaction."
   [& deps]
+  (log/info (format "satisfying deps: %s" deps))
   (send-off dep-state* satisfy* (set deps)))
 
 (defn reset-deps
@@ -133,19 +137,22 @@
 
 (defn wait-until-deps-satisfied
   "Makes the current thread sleep until specified deps have been satisfied.
-  Thread enters a sleep cycle sleeping for wait-time ms before each dep check.
+  Thread enters a sleep cycle sleeping for wait-time seconds before each dep check.
   If timeout is a positive value throws timeout exception if deps haven't been
-  satisfied by timeout ms. Default wait-time is 100ms and default timeout is
-  10000 ms"
-  ([deps] (wait-until-deps-satisfied deps 10000 100))
-  ([deps timeout] (wait-until-deps-satisfied deps timeout 100))
+  satisfied by timeout secs. The default wait-time is 0.1 seconds, and the
+  default timeout is 10 seconds."
+  ([deps] (wait-until-deps-satisfied deps 10 0.1))
+  ([deps timeout] (wait-until-deps-satisfied deps timeout 0.1))
   ([deps timeout wait-time]
+   (let [timeout (* 1000 timeout)
+         wait-time (* 1000 wait-time)]
      (if (<= timeout 0)
        (while (not (deps-satisfied? deps))
          (Thread/sleep wait-time))
        (loop [sleep-time 0]
          (when (> sleep-time timeout)
-           (throw (Exception. (str "The following deps took too long to be satisfied: " deps))))
+           (throw (Exception. (str "The following deps took too long (" timeout
+                                   " seconds) to be satisfied: " deps))))
          (when-not (deps-satisfied? deps)
            (Thread/sleep wait-time)
-           (recur (+ sleep-time wait-time)))))))
+           (recur (+ sleep-time wait-time))))))))
