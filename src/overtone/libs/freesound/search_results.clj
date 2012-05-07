9afab4bcab1a13e065db2c1537114b72aa7d5823
Kevin Neaton <neatonk@gmail.com>
2012-05-06 19:16:59 -0500

    Added freesound-search and freesound-search-paths
    Now you can search freesound.org and get back a single lazy-seq of results
    spanning one or more api-requests. Use freesound-search-paths to download and
    cache the matching sounds as in asset-path.
    
    * Implemented a custom SearchResults type to make the return value of
      freesound-search nicer to work with. You can get the LazySeq behind behind a
      SearchResults instance by just deref'ing it.
    
    * api-seq and lazy-cats are used to build a lazy-seq of search results across
      one or more api calls.
    
    Also added...
    * freesound-searchm
    * slurp-json, slurp-json-mem, and slurp-json-asset
    * url helper fn's

---
+++ src/overtone/libs/freesound/search_results.clj
@@ -0,0 +1,76 @@
+(ns ^{:doc "A custom datatype and helper functions for working with a lazy sequence of search results."
+      :author "Kevin Neaton"}
+  overtone.libs.freesound.search-results
+  (:use [overtone.libs.freesound.url :only [build-url]]))
+
+(deftype SearchResults [n-results results-seq]
+  clojure.lang.Sequential
+  clojure.lang.Seqable
+  (seq [this] this)
+
+  clojure.lang.Counted
+  (count [_] n-results)
+
+  clojure.lang.IPersistentCollection
+  (cons [_ x]
+    (SearchResults. n-results (.cons results-seq x)))
+  (empty [_]
+    (.empty results-seq))
+  (equiv [this that]
+    (and (= (.count this)
+            (.count that))
+         (.equiv results-seq (.results that))))
+
+  clojure.lang.ISeq
+  (first [_]
+    (.first results-seq))
+  (next [this]
+    (when (> n-results 1)
+      (SearchResults. (dec n-results) (.next results-seq))))
+  (more [_]
+    (when (> n-results 1)
+      (SearchResults. (dec n-results) (.more results-seq))))
+
+  clojure.lang.IPending
+  (isRealized [_] (.isRealized results-seq))
+
+  clojure.lang.IDeref
+  (deref [_] results-seq))
+
+(defmethod print-method SearchResults
+  [x writer]
+  (.write writer (str x)))
+
+(defn search-results
+  "Create a new instance of SearchResults."
+  [n-results results-seq]
+  {:pre [(integer? n-results)
+         (sequential? results-seq)]}
+  (SearchResults. n-results (lazy-seq results-seq)))
+
+(defn next-fn
+  "Returns a function that takes a freesound api response map and returns the
+  'next' url with the given params or nil. This is needed to compensate for an
+  api bug which causes some pagination uri's to be incomplete."
+  [key params]
+  (let [params (assoc params :api_key key)]
+    (fn [resp]
+      (when-let [next-url (:next resp)]
+        (build-url next-url params)))))
+
+(defn api-seq
+  "Returns a lazy seq of paginated api responses. Retrieves the resource at
+  'url' and passes the response to 'next-fn', which should return the next-url
+  or nil."
+  [url api-fn next-fn]
+  (lazy-seq
+   (let [resp (api-fn url)]
+     (cons resp (when-let [next-url (next-fn resp)]
+                  (api-seq next-url api-fn next-fn))))))
+
+(defn lazy-cats
+  "Returns a lazy seq over the concatenation the items in coll."
+  [coll]
+  (lazy-seq
+   (when-let [s (seq coll)]
+     (concat (first s) (lazy-cats (rest s))))))
