6e5cacebf44ec401cae80ff00eb967cd6a4050d5
Sam Aaron <samaaron@gmail.com>
2012-05-18 21:41:56 +0100

    Work on auto-midi setup.
    * Ensure evaluating ns multiple times doesn't have any unwanted side effects by using defence where appropriate.
    * Fix issues where midi code is written for an unreleased version of midi-clj
    * Create additional midi-device specific event for handling events from a specific device
    * Add scheduled fn description

--- src/overtone/studio/midi.clj
+++ src/overtone/studio/midi.clj
@@ -5,50 +5,55 @@
         [overtone.libs.event])
   (:require [overtone.config.log :as log]))
 
-(def MIDI-POOL (mk-pool))
+(defonce MIDI-POOL (mk-pool))
+(defonce midi-devices* (atom {}))
 
 (def EXCLUDED-DEVICES #{"Real Time Sequencer" "Java Sound Synthesizer"})
 
-(def midi-devices* (atom {}))
-
 (defn midi-event
   "Trigger a global midi event."
-  [msg ts]
-  (event [:midi (:command msg)] msg))
+  [dev msg ts]
+  (event [:midi (msg :command)] msg)
+  (event [:midi-device (dev :vendor) (dev :name) (dev :description) (msg :command)] msg))
 
 (defn- detect-midi-devices
+  "Designed to run periodically and update the midi-devices* atom with
+  the latest list of midi sources."
   []
   (try
-    (let [old-devs (set (keys @midi-devices*))
-          devs (midi-sources)
-          devs (filter #(not (old-devs (:device %))) devs)
-          receivers (doall (filter
-                             (fn [dev]
-                               (try
-                                 (midi-handle-events (midi-in dev) #'midi-event)
-                                 (catch Exception e
-                                   (log/warn "Can't listen to midi devicee: " dev "\n" e)
-                                   false)))
-                             devs))
+    (let [old-devs     (set (keys @midi-devices*))
+          devs         (midi-sources)
+          devs         (filter #(not (old-devs (:device %))) devs)
+          receivers    (doall (filter
+                               (fn [dev]
+                                 (try
+                                   (midi-handle-events (midi-in dev) #(midi-event dev %1 %2))
+                                   true
+                                   (catch Exception e
+                                     (log/warn "Can't listen to midi device: " dev "\n" e)
+                                     false)))
+                               devs))
           device-names (map :name devs)
-          n-devs (count device-names)
-          dev-map (apply hash-map (interleave (map :device devs) receivers))]
+          n-devs       (count device-names)
+          dev-map      (apply hash-map (interleave (map :device devs) receivers))]
       (swap! midi-devices* merge dev-map)
       (when (pos? n-devs)
-        (log/info "Connected " n-devs " midi devices: " device-names)))
+         (log/info "Connected " n-devs " midi devices: " device-names)))
     (catch Exception ex
       (println "Got exception in detect-midi-devices!" ex))))
 
 ; The rate at which we poll for new midi devices
 (def MIDI-POLL-RATE 2000)
 
-(def device-poller (every MIDI-POLL-RATE #'detect-midi-devices MIDI-POOL))
+(defonce __DEVICE-POLLER__
+  (every MIDI-POLL-RATE #'detect-midi-devices MIDI-POOL :desc "Check for new midi devices"))
 
 (defn midi-poly-player
-  "Sets up the event handlers and manages synth instances to easily play a polyphonic
-  instrument with a midi controller.  The play-fn should take the note and velocity as
-  the only two arguments, and the synth should have a gate parameter that can be set to
-  zero when a :note-off event is received.
+  "Sets up the event handlers and manages synth instances to easily play
+  a polyphonic instrument with a midi controller.  The play-fn should
+  take the note and velocity as the only two arguments, and the synth
+  should have a gate parameter that can be set to zero when a :note-off
+  event is received.
 
     (definst ding
       [note 60 velocity 100 gate 1]
@@ -58,8 +63,7 @@
             env  (env-gen (adsr 0.001 0.1 0.6 0.3) gate :action FREE)]
         (* amp env snd)))
 
-    (def dinger (midi-poly-player ding))
-  "
+    (def dinger (midi-poly-player ding))"
   [play-fn]
   (let [notes*  (atom {})
         on-key  (keyword (gensym 'note-on))
