1b9fec355ecfff3c25e715e7b83ee34c0e919bff
Sam Aaron <samaaron@gmail.com>
2012-06-26 21:09:50 +0100

    Collaborative Programmable Music

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject overtone "0.8.0-SNAPSHOT"
-  :description "Programmable Music."
+  :description "Collaborative Programmable Music."
   :url "http://project-overtone.org"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.incubator "0.1.0"]
