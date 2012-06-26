1ab54e9f6ed6ff1dde69987ddc9dd03faf410103
Sam Aaron <samaaron@gmail.com>
2012-06-26 19:08:24 +0100

    tidy up docstring

--- examples/wobble.clj
+++ examples/wobble.clj
@@ -2,7 +2,8 @@
   (:use [overtone.live]))
 
 (defcgen wobble
-  "wobble an input source to a specified wobble val (which is designed to be remotely modulated"
+  "wobble an input source to a specified wobble val (which is designed
+  to be remotely modulated"
   [src {:doc "input source"}
    wobble-amount {:doc "Amount of wobble to apply (-1 to 1)" :default 0}
    min {:doc "minimum freq to always let through" :default 40}
