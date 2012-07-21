7ca153c4e91b9dac08b3190a448ef0d922908b16
Sam Aaron <samaaron@gmail.com>
2012-07-21 20:23:41 +0100

    temporarily remove buf-wr checking - needs to be more lenient.

--- src/overtone/sc/machinery/ugen/metadata/buf_io.clj
+++ src/overtone/sc/machinery/ugen/metadata/buf_io.clj
@@ -61,7 +61,7 @@
               {:name "bufnum", :default 0 :doc "the index of the buffer to use"}
               {:name "phase", :default 0.0 :doc "modulatable index into the buffer (has to be audio rate)."}
               {:name "loop", :default 1.0 :doc "1 means true, 0 means false.  This is modulatable"}]
-       :check (when-ar (nth-input-ar 1))
+;;       :check (when-ar (nth-input-ar 1))
        :doc "writes to a buffer at a given index. Note, buf-wr (in difference to buf-rd) does not do multichannel expansion, because input is an array."}
 
       {:name "RecordBuf",
