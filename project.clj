da2c786c638ec2eba5eafd1ba6fcb041553b9bd6
Jeff Rose <jeff@deepmind.com>
2012-07-27 16:24:15 +0100

    Conditionally modifying JVM args depending on OS.
    Passing the OSX specific dock:name argument to the JVM causes problems for non
    mac JVMs.  With this modification we can now support arbitary, per-os
    differences in the JVM options.

--- project.clj
+++ project.clj
@@ -1,3 +1,44 @@
+(defn get-os
+    "Return the OS as a keyword. One of :windows :linux :max"
+    []
+    (let [os (System/getProperty "os.name")]
+          (cond
+                  (re-find #"[Ww]indows" os) :windows
+                  (re-find #"[Ll]inux" os)   :linux
+                  (re-find #"[Mm]ac" os)     :mac)))
+
+(def JVM-OPTIONS
+  (concat
+  ["-Xms512m" "-Xmx1g"           ; Minimum and maximum sizes of the heap
+   "-XX:+UseParNewGC"            ; Use the new parallel GC in conjunction with
+   "-XX:+UseConcMarkSweepGC"     ;  the concurrent garbage collector
+   "-XX:+CMSConcurrentMTEnabled" ; Enable multi-threaded concurrent gc work (ParNewGC)
+   "-XX:MaxGCPauseMillis=20"     ; Specify a target of 20ms for max gc pauses
+   "-XX:+CMSIncrementalMode"     ; Do many small GC cycles to minimize pauses
+   "-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
+   "-XX:NewSize=256m"            ;  generation to be small
+   "-XX:+UseTLAB"                ; Uses thread-local object allocation blocks. This
+                                 ;  improves concurrency by reducing contention on
+                                 ;  the shared heap lock.
+   "-XX:MaxTenuringThreshold=0"  ; Makes the full NewSize available to every NewGC
+
+   ;  cycle, and reduces the pause time by not
+   ;  evaluating tenured objects. Technically, this
+   ;  setting promotes all live objects to the older
+   ;  generation, rather than copying them.
+   ;;  "-XX:ConcGCThreads=2"        ; Use 2 threads with concurrent gc collections
+   ;;  "-XX:TieredCompilation"      ; JVM7 - combine both client and server compilation
+   ;;                               ;  strategies
+   ;;  "-XX:CompileThreshold=1"     ; JIT each function after one execution
+   ;;  "-XX:+PrintGC"               ; Print GC info to stdout
+   ;;  "-XX:+PrintGCDetails"        ;  - with details
+   ;;  "-XX:+PrintGCTimeStamps"     ;  - and timestamps
+   ]
+    (case (get-os)
+      :mac ["-Xdock:name=Overtone"]
+      [])))
+
+
 (defproject overtone "0.8.0-SNAPSHOT"
   :description "Collaborative Programmable Music."
   :url "http://project-overtone.org"
@@ -15,30 +56,4 @@
                  [org.clojure/core.match "0.2.0-alpha9"]
                  [seesaw "1.4.1"]]
   :native-path "native"
-  :jvm-opts
-  ["-Xdock:name=Overtone"
-   "-Xms512m" "-Xmx1g"           ; Minimum and maximum sizes of the heap
-   "-XX:+UseParNewGC"            ; Use the new parallel GC in conjunction with
-   "-XX:+UseConcMarkSweepGC"     ;  the concurrent garbage collector
-   "-XX:+CMSConcurrentMTEnabled" ; Enable multi-threaded concurrent gc work (ParNewGC)
-   "-XX:MaxGCPauseMillis=20"     ; Specify a target of 20ms for max gc pauses
-   "-XX:+CMSIncrementalMode"     ; Do many small GC cycles to minimize pauses
-   "-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
-   "-XX:NewSize=256m"            ;  generation to be small
-   "-XX:+UseTLAB"                ; Uses thread-local object allocation blocks. This
-                                 ;  improves concurrency by reducing contention on
-                                 ;  the shared heap lock.
-   "-XX:MaxTenuringThreshold=0"  ; Makes the full NewSize available to every NewGC
-                                 ;  cycle, and reduces the pause time by not
-                                 ;  evaluating tenured objects. Technically, this
-                                 ;  setting promotes all live objects to the older
-                                        ;  generation, rather than copying them.
-;;  "-XX:ConcGCThreads=2"        ; Use 2 threads with concurrent gc collections
-;;  "-XX:TieredCompilation"      ; JVM7 - combine both client and server compilation
-;;                               ;  strategies
-;;  "-XX:CompileThreshold=1"     ; JIT each function after one execution
-;;  "-XX:+PrintGC"               ; Print GC info to stdout
-;;  "-XX:+PrintGCDetails"        ;  - with details
-;;  "-XX:+PrintGCTimeStamps"     ;  - and timestamps
-]
-)
+  :jvm-opts ~JVM-OPTIONS)
