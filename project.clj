0a87ad2883a15bc516691ca4dedb4d9bec474350
Kevin Neaton <neatonk@gmail.com>
2012-07-27 10:48:59 -0500

    Specify JVM args per-os...
    The macosx specific `dock:name` option was preventing the JVM from
    launching in linux.

--- project.clj
+++ project.clj
@@ -1,3 +1,44 @@
+(require 'leiningen.core.eval)
+
+(def JVMOPTS
+  "Per os jvm options. Options common to all cases go under
+  `:any`. Options specific to one OS go under the key returned by
+  `leiningen.core.eval/get-os` for that system. Temporarily disabled
+  options can be kept under `:disabled`."
+  {:any
+   ["-Xms512m" "-Xmx1g"           ; Minimum and maximum sizes of the heap
+    "-XX:+UseParNewGC"            ; Use the new parallel GC in conjunction with
+    "-XX:+UseConcMarkSweepGC"     ;  the concurrent garbage collector
+    "-XX:+CMSConcurrentMTEnabled" ; Enable multi-threaded concurrent gc work (ParNewGC)
+    "-XX:MaxGCPauseMillis=20"     ; Specify a target of 20ms for max gc pauses
+    "-XX:+CMSIncrementalMode"     ; Do many small GC cycles to minimize pauses
+    "-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
+    "-XX:NewSize=256m"            ;  generation to be small
+    "-XX:+UseTLAB"                ; Uses thread-local object allocation blocks. This
+                                  ;  improves concurrency by reducing contention on
+                                  ;  the shared heap lock.
+    "-XX:MaxTenuringThreshold=0"] ; Makes the full NewSize available to every NewGC
+                                  ;  cycle, and reduces the pause time by not
+                                  ;  evaluating tenured objects. Technically, this
+                                  ;  setting promotes all live objects to the older
+                                  ;  generation, rather than copying them.
+   :macosx
+   ["-Xdock:name=Overtone"]
+   :disabled
+   ["-XX:ConcGCThreads=2"         ; Use 2 threads with concurrent gc collections
+    "-XX:TieredCompilation"       ; JVM7 - combine both client and server compilation
+                                  ;  strategies
+    "-XX:CompileThreshold=1"      ; JIT each function after one execution
+    "-XX:+PrintGC"                ; Print GC info to stdout
+    "-XX:+PrintGCDetails"         ;  - with details
+    "-XX:+PrintGCTimeStamps"]})   ;  - and timestamps
+
+(defn jvm-opts
+  "Return a complete vector of jvm-opts for the current os."
+  [] (let [os (leiningen.core.eval/get-os)]
+       (vec (set (concat (get JVMOPTS :any)
+                         (get JVMOPTS os))))))
+
 (defproject overtone "0.8.0-SNAPSHOT"
   :description "Collaborative Programmable Music."
   :url "http://project-overtone.org"
@@ -15,30 +56,4 @@
                  [org.clojure/core.match "0.2.0-alpha9"]
                  [seesaw "1.4.1"]]
   :native-path "native"
-  :jvm-opts
-  ["-Xdock:name=Overtone"
-   "-Xms512m" "-Xmx1g"           ; Minimum and maximum sizes of the heap
-   "-XX:+UseParNewGC"            ; Use the new parallel GC in conjunction with
-   "-XX:+UseConcMarkSweepGC"     ;  the concurrent garbage collector
-   "-XX:+CMSConcurrentMTEnabled" ; Enable multi-threaded concurrent gc work (ParNewGC)
-   "-XX:MaxGCPauseMillis=20"     ; Specify a target of 20ms for max gc pauses
-   "-XX:+CMSIncrementalMode"     ; Do many small GC cycles to minimize pauses
-   "-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
-   "-XX:NewSize=256m"            ;  generation to be small
-   "-XX:+UseTLAB"                ; Uses thread-local object allocation blocks. This
-                                 ;  improves concurrency by reducing contention on
-                                 ;  the shared heap lock.
-   "-XX:MaxTenuringThreshold=0"  ; Makes the full NewSize available to every NewGC
-                                 ;  cycle, and reduces the pause time by not
-                                 ;  evaluating tenured objects. Technically, this
-                                 ;  setting promotes all live objects to the older
-                                        ;  generation, rather than copying them.
-;;  "-XX:ConcGCThreads=2"        ; Use 2 threads with concurrent gc collections
-;;  "-XX:TieredCompilation"      ; JVM7 - combine both client and server compilation
-;;                               ;  strategies
-;;  "-XX:CompileThreshold=1"     ; JIT each function after one execution
-;;  "-XX:+PrintGC"               ; Print GC info to stdout
-;;  "-XX:+PrintGCDetails"        ;  - with details
-;;  "-XX:+PrintGCTimeStamps"     ;  - and timestamps
-]
-)
+  :jvm-opts ~(jvm-opts))
