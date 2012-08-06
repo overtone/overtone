280e75aae709d2c98d055d036bab93b16c5f9ec7
Sam Aaron <samaaron@gmail.com>
2012-08-06 21:46:04 +0100

    move beats example to getting-started ns and rename to rhythm

---
+++ src/overtone/examples/getting_started/rhythm.clj
@@ -0,0 +1,27 @@
+(ns overtone.examples.getting-started.rhythm
+  (:use [overtone.live]
+        [overtone.inst.drum :only [quick-kick haziti-clap soft-hat]]))
+
+(def m (metronome 128))
+
+(defn player
+  [beat]
+  (let [next-beat (inc beat)]
+    (at (m beat)
+        (quick-kick :amp 0.5)
+        (if (zero? (mod beat 2))
+          (hat :amp 0.1)))
+    (at (m (+ 0.5 beat))
+        (haziti-clap :decay 0.05 :amp 0.3))
+
+    (when (zero? (mod beat 3))
+      (at (m (+ 0.75 beat))
+          (soft-hat :decay 0.03 :amp 0.2)))
+
+    (when (zero? (mod beat 8))
+      (at (m (+ 1.25 beat))
+          (soft-hat :decay 0.03)))
+
+    (apply-at (m next-beat) #'player [next-beat])))
+
+;(player (m))
