b73a2281b7bda47d5701533f19abd1f92de4c65d
Sam Aaron <samaaron@gmail.com>
2012-08-08 23:17:04 +0100

    remove duplication between overtone.core and overtone.live

--- src/overtone/core.clj
+++ src/overtone/core.clj
@@ -14,60 +14,64 @@
             [overtone.libs asset event freesound]
             [overtone.gui scope mixer control]))
 
-(immigrate
-  'overtone.osc
-  'overtone.midi
-  'overtone.algo.chance
-  'overtone.algo.scaling
-  'overtone.algo.trig
-  'overtone.config.store
-  'overtone.sc.bindings
-  'overtone.sc.buffer
-  'overtone.sc.bus
-  'overtone.sc.envelope
-  'overtone.sc.example
-  'overtone.sc.info
-  'overtone.sc.node
-  'overtone.sc.sample
-  'overtone.sc.server
-  'overtone.sc.synth
-  'overtone.sc.trigger
-  'overtone.sc.ugens
-  'overtone.sc.defcgen
-  'overtone.sc.cgens.oscillators
-  'overtone.sc.cgens.demand
-  'overtone.sc.cgens.mix
-  'overtone.sc.cgens.io
-  'overtone.sc.cgens.buf-io
-  'overtone.sc.cgens.env
-  'overtone.sc.cgens.tap
-  'overtone.sc.cgens.line
-  'overtone.sc.cgens.freq
-  'overtone.studio.mixer
-  'overtone.studio.inst
-  'overtone.studio.fx
-  'overtone.studio.wavetable
-  'overtone.studio.midi
-  'overtone.studio.core
-  'overtone.music.rhythm
-  'overtone.music.pitch
-  'overtone.music.tuning
-  'overtone.music.time
-  'overtone.speech
-  'overtone.repl.ugens
-  'overtone.repl.examples
-  'overtone.repl.shell
-  'overtone.repl.inst
-  'overtone.repl.debug
-  'overtone.libs.asset
-  'overtone.libs.event
-  'overtone.libs.freesound
-  'overtone.version)
+(defn immigrate-overtone-core []
+  (immigrate
+   'overtone.osc
+   'overtone.midi
+   'overtone.algo.chance
+   'overtone.algo.scaling
+   'overtone.algo.trig
+   'overtone.config.store
+   'overtone.sc.bindings
+   'overtone.sc.buffer
+   'overtone.sc.bus
+   'overtone.sc.envelope
+   'overtone.sc.example
+   'overtone.sc.info
+   'overtone.sc.node
+   'overtone.sc.sample
+   'overtone.sc.server
+   'overtone.sc.synth
+   'overtone.sc.trigger
+   'overtone.sc.ugens
+   'overtone.sc.defcgen
+   'overtone.sc.cgens.oscillators
+   'overtone.sc.cgens.demand
+   'overtone.sc.cgens.mix
+   'overtone.sc.cgens.io
+   'overtone.sc.cgens.buf-io
+   'overtone.sc.cgens.env
+   'overtone.sc.cgens.tap
+   'overtone.sc.cgens.line
+   'overtone.sc.cgens.freq
+   'overtone.studio.mixer
+   'overtone.studio.inst
+   'overtone.studio.fx
+   'overtone.studio.wavetable
+   'overtone.studio.midi
+   'overtone.studio.core
+   'overtone.music.rhythm
+   'overtone.music.pitch
+   'overtone.music.tuning
+   'overtone.music.time
+   'overtone.speech
+   'overtone.repl.ugens
+   'overtone.repl.examples
+   'overtone.repl.shell
+   'overtone.repl.inst
+   'overtone.repl.debug
+   'overtone.libs.asset
+   'overtone.libs.event
+   'overtone.libs.freesound
+   'overtone.version))
 
-(when-not (server-connected?)
-  (println "--> Please boot a server to start making noise:
+(immigrate-overtone-core)
+
+(defonce __PRINT-CONNECT-HELP__
+  (when-not (server-connected?)
+    (println "--> Please boot a server to start making noise:
     * (boot-server)             ; boot default server (honours config)
     * (boot-internal-server)    ; boot an internal server
     * (boot-external-server)    ; boot an external server
     * (connect-external-server) ; connect to an existing external server
-"))
+")))
