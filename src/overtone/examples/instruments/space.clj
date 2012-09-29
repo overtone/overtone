800630090cb75fd4c52ed016a54570839c8c8049
Sam Aaron <samaaron@gmail.com>
2012-09-29 23:31:33 +0100

    add space reverb example synth

--- src/overtone/examples/instruments/space.clj
+++ src/overtone/examples/instruments/space.clj
@@ -1,6 +1,9 @@
 (ns overtone.examples.instruments.space
   (:use overtone.live))
 
+;; Recipe from Mitchell Sigman (2011) Steal this Sound. Milwaukee, WI: Hal Leonard Books
+;; Adapted from a translated version by Nick Collins
+
 (defsynth space-theremin
   [out-bus 0
    freq 440
@@ -18,3 +21,33 @@
         filter (b-low-pass4 osc (lag cutoff (* lag-time 4)) rq)
         env    (env-gen:ar (adsr 0.6 0 1 0.05) gate FREE)]
     (out out-bus (pan2 (* filter env (lag amp (* 4 lag-time))) pan))))
+
+
+
+(defsynth space-reverb
+  [out-bus 0
+   in-bus 0
+   gate 1
+   threshold 0.1
+   amp 0.1]
+  (let [env             (linen gate 0.1 1 0.1 FREE)
+        num-combs       6
+        num-allpass     4
+        src             (in in-bus 2)
+        reverb-predelay (delay-n src 0.048 0.048)
+        y               (mix (repeat num-combs (comb-l reverb-predelay 0.1 (ranged-rand 0.01 0.1) 5)))
+        y               (loop [cnt num-allpass
+                               res y]
+                          (if (<= cnt 0)
+                            res
+                            (recur (dec cnt)
+                                   (allpass-n res
+                                              0.051
+                                              [(ranged-rand 0.01 0.05)
+                                               (ranged-rand 0.01 0.05)]
+                                              1))))]
+    (out out-bus (* env amp (pan2 y)))))
+
+;;(space-theremin :pos :head :out-bus 10 :amp 0.8 :cutoff 1000)
+;;(space-reverb :pos :tail :in-bus 10)
+;;(stop)
