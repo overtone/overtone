59b8a7c109a6097d080072dc44edd10249160f96
Sam Aaron <samaaron@gmail.com>
2012-10-01 21:08:51 +0100

    tidy up some trig ugens

--- src/overtone/sc/machinery/ugen/metadata/trig.clj
+++ src/overtone/sc/machinery/ugen/metadata/trig.clj
@@ -11,12 +11,7 @@
   [
 
    {:name "TWindex"
-    :description "Triggered window"
-    :doc "When triggered, returns a random index value based on array as
-          a list of probabilities. By default the list of probabilities
-          should sum to 1.0, when the normalize flag is set to 1, the
-          values get normalized by the ugen (less efficient)."
-
+    :summary "Triggered window"
     :args [{:name "trig"
             :doc "Trigger - can be any signal. A trigger happens when
                   the signal changes from non-positive to positive." }
@@ -26,55 +21,78 @@
             :array true }
 
            {:name "normalize",
-            :doc "normalise flag - 0 off, 1 on"
-            :default 0}]
+            :default 0
+            :doc "normalise flag - 0 off, 1 on"}]
+    :doc "When triggered, returns a random index value based on array as
+          a list of probabilities. By default the list of probabilities
+          should sum to 1.0, when the normalize flag is set to 1, the
+          values get normalized by the ugen (less efficient)."
+
+
     :default-rate :kr}
 
 
    {:name "Trig1"
-    :description "Timed trigger"
-    :doc "Outputs one for dur seconds whenever the input goes from
-          negative to positive, otherwise outputs 0."
-
+    :summary "Timed trigger"
     :args [{:name "trig",
+            :default 0.0
             :doc "trigger. Trigger can be any signal. A trigger happens
                   when the signal changes from non-positive to
-                  positive."
-            :default 0.0}
+                  positive."}
 
            {:name "dur",
-            :doc  "duration of the trigger output in seconds."
-            :default 0.1 }]
+            :default 0.1
+            :doc  "duration of the trigger output in seconds."}]
     :signal-range :unipolar
-    :default-rate :kr}
+    :default-rate :kr
+    :doc "Outputs one for dur seconds whenever the input goes from
+          negative to positive, otherwise outputs 0."}
 
 
    {:name "Trig" :extends "Trig1"
-    :description "Timed trigger"
+    :summary "Timed trigger"
     :doc "When a nonpositive to positive transition occurs at the input,
           Trig outputs the level of the triggering input for the
           specified duration, otherwise it outputs zero."}
 
 
    {:name "TDelay"
-    :description "Trigger delay"
-    :doc "Delays an input trigger by dur, ignoring other triggers in the
-          meantime"
-
+    :summary "Trigger delay"
     :args [{:name "trig",
-            :doc "input trigger signal."
-            :default 0.0 }
+            :default 0.0
+            :doc "input trigger signal."}
 
            {:name "dur"
-            :doc "delay time in seconds."
-            :default 0.1 }]
+            :default 0.1
+            :doc "delay time in seconds."}]
     :signal-range :unipolar
     :default-rate :kr
-    :check [(same-rate-as-first-input)]}
+    :check [(same-rate-as-first-input)]
+    :doc "Delays an input trigger by dur, ignoring other triggers in the
+          meantime"}
 
 
    {:name "SendTrig"
-    :description "Send a /tr OSC message to Overtone"
+    :summary "Send a /tr OSC message to Overtone"
+    :args [{:name "in"
+            :default 0.0
+            :doc "input trigger signal"}
+
+           {:name "id"
+            :default 0
+            :doc "an integer that will be passed with the trigger
+                  message. This is useful if you have more than one
+                  SendTrig in a SynthDef"}
+
+           {:name "value"
+            :default 0.0
+            :doc "A UGen or float that will be polled at the time of
+                  trigger, and its value passed with the trigger
+                  message"}]
+
+    :default-rate :kr
+    :num-outs 0
+    :check [(same-rate-as-first-input)]
     :doc "On receiving a trigger sends a :trigger event with id and
           value. This command is the mechanism that synths can use to
           trigger events in clients.
@@ -87,30 +105,29 @@
 
           int   - trigger ID
 
-          float - trigger value"
-
-    :args [{:name "in"
-            :doc "input trigger signal"
-            :default 0.0 }
-
-           {:name "id"
-            :doc "an integer that will be passed with the trigger
-                  message. This is useful if you have more than one
-                  SendTrig in a SynthDef"
-            :default 0 }
-
-           {:name "value"
-            :doc "A UGen or float that will be polled at the time of
-                  trigger, and its value passed with the trigger
-                  message"
-            :default 0.0 }]
-    :default-rate :kr
-    :num-outs 0
-    :check [(same-rate-as-first-input)]}
+          float - trigger value"}
 
 
    {:name "SendReply"
-    :description "Send information via OSC to Overtone"
+    :summary "Send information via OSC to Overtone"
+    :args [{:name "trig"
+            :default 0.0
+            :doc "Input trigger signal"}
+
+           {:name "cmd-name"
+            :default "/reply"
+            :doc "A string or symbol, as a message name."
+            :mode :append-string }
+
+           {:name "values"
+            :default 0.0
+            :doc "Array of ugens, or valid ugen inputs"
+            :mode :append-sequence }
+
+           {:name "reply-id"
+            :default -1
+            :doc "Integer id (similar to that used by send-trig)"}]
+    :default-rate :kr
     :doc "Send an array of values from the server via an
           message. The OSC message is formed with cmd-name as the path,
           followed by two compulsary args: node-id (the id of the node
@@ -130,65 +147,47 @@
 
           You can register to respond to this event as follows:
 
-          (on-event \"/foobar\" (fn [msg] (println msg)) ::handle-foobar)"
-    :args [{:name "trig"
-            :doc "Input trigger signal"
-            :default 0.0 }
-
-           {:name "cmd-name"
-            :doc "A string or symbol, as a message name."
-            :default "/reply"
-            :mode :append-string }
-
-           {:name "values"
-            :doc "Array of ugens, or valid ugen inputs"
-            :default 0.0
-            :mode :append-sequence }
-
-           {:name "reply-id"
-            :doc "Integer id (similar to that used by send-trig)"
-            :default -1 }]
-    :default-rate :kr}
+          (on-event \"/foobar\" (fn [msg] (println msg)) ::handle-foobar)"}
 
 
    {:name "Latch"
-    :description "Sample and hold"
-    :doc "Holds input signal value when triggered."
+    :summary "Sample and hold"
     :args [{:name "in"
-            :doc "Input signal."
-            :default 0.0 }
+            :default 0.0
+            :doc "Input signal."}
 
            {:name "trig"
+            :default 0.0
             :doc "Trigger. Trigger can be any signal. A trigger happens
                   when the signal changes from non-positive to
-                  positive."
-            :default 0.0}]
-    :default-rate :kr}
+                  positive."}]
+    :default-rate :kr
+    :doc "Holds input signal value when triggered."}
 
 
    {:name "Gate" :extends "Latch"
-    :description "Gate or hold"
+    :summary "Gate or hold"
     :doc "Lets signal flow when trig is positive, otherwise holds last
           input value"}
 
 
    {:name "PulseCount"
-    :description "Pulse counter"
-    :doc "Each input trigger increments a counter value that is output."
+    :summary "Pulse counter"
     :args [{:name "trig"
+            :default 0.0
             :doc "Trigger. Trigger can be any signal. A trigger happens
-                  when the signal changes from non-positive to positive."
-            :default 0.0 }
+                  when the signal changes from non-positive to positive."}
 
            {:name "reset"
-            :doc "Resets the counter to zero when triggered."
-            :default 0.0 }]
+            :default 0.0
+            :doc "Resets the counter to zero when triggered."}]
     :check (same-rate-as-first-input)
-    :default-rate :kr}
+    :default-rate :kr
+    :doc "Each input trigger increments a counter value that is output."}
 
 
    {:name "SetResetFF", :extends "PulseCount"
-    :description "Set-reset flip flop"
+    :summary "Set-reset flip flop"
     :doc "When a trigger is received the output is set to 1.0 Subsequent
           triggers have no effect When a trigger is received in the
           reset input, the output is set back to 0.0
@@ -198,27 +197,27 @@
 
 
    {:name "Peak", :extends "PulseCount"
-    :description "Track peak signal amplitude"
+    :summary "Track peak signal amplitude"
     :doc "Outputs the peak amplitude of the signal so far, a trigger
           resets to current value"}
 
 
    {:name "RunningMin", :extends "Latch"
-    :description "Track minimum level"
+    :summary "Track minimum level"
     :doc "Outputs the minimum value received at the input. When
           triggered, the minimum output value is reset to the current
           value." }
 
 
    {:name "RunningMax", :extends "Latch"
-    :description "Track maximum level"
+    :summary "Track maximum level"
     :doc "Outputs the maximum value received at the input. When
           triggered, the maximum output value is reset to the current
           value." }
 
 
    {:name "Stepper"
-    :description "Pulse counter"
+    :summary "Pulse counter"
     :doc "Triggers increment a counter which is output as a signal. The
           counter loops around from max to min by step increments"
 
@@ -252,7 +251,7 @@
 
 
    {:name "PulseDivider"
-    :description "Pulse divider"
+    :summary "Pulse divider"
     :doc "Outputs a trigger every div input triggers"
     :args [{:name "trig"
             :doc "Trigger. Trigger can be any signal. A trigger happens
@@ -274,7 +273,7 @@
 
 
    {:name "ToggleFF"
-    :description "Toggle flip flop"
+    :summary "Toggle flip flop"
     :doc "Flip-flops between zero and one each trigger"
     :args [{:name "trig"
             :doc "trigger input"
@@ -283,7 +282,7 @@
 
 
    {:name "ZeroCrossing"
-    :description "Zero crossing frequency follower"
+    :summary "Zero crossing frequency follower"
     :doc "Outputs a frequency based upon the distance between
           interceptions of the X axis. The X intercepts are determined
           via linear interpolation so this gives better than just
@@ -297,7 +296,7 @@
 
 
    {:name "Timer"
-    :description "Trigger timer"
+    :summary "Trigger timer"
     :doc "Outputs time since last trigger"
 
     :args [{:name "trig"
@@ -308,7 +307,7 @@
 
 
    {:name "Sweep"
-    :description "Triggered linear ramp"
+    :summary "Triggered linear ramp"
     :doc "outputs a linear increasing signal by rate/second when trig input crosses from non-positive to positive"
 
     :args [{:name "trig"
@@ -322,7 +321,7 @@
 
 
    {:name "Phasor"
-    :description "Resettable linear ramp between two levels"
+    :summary "Resettable linear ramp between two levels"
     :doc "Phasor is a linear ramp between start and end values. When its
           trigger input crosses from non-positive to positive, Phasor's
           output will jump to its reset position. Upon reaching the end
@@ -356,7 +355,7 @@
 
 
    {:name "PeakFollower"
-    :description "Track peak signal amplitude"
+    :summary "Track peak signal amplitude"
     :doc "Outputs the peak signal amplitude, falling with decay over
          time until reaching signal level"
     :args [{:name "in"
@@ -370,7 +369,7 @@
 
 
    {:name "Pitch"
-    :description "Autocorrelation pitch follower"
+    :summary "Autocorrelation pitch follower"
     :doc "This is a better pitch follower than zero-crossing, but more
           costly of CPU. For most purposes the default settings can be
           used and only in needs to be supplied. Pitch returns two
@@ -440,7 +439,7 @@
 
 
    {:name "InRange"
-    :description "Tests if a signal is within a given range"
+    :summary "Tests if a signal is within a given range"
     :doc "If in is >= lo and <= hi output 1.0, otherwise output
           0.0. Output is initially zero."
     :args [{:name "in"
@@ -457,13 +456,13 @@
     :default-rate :kr}
 
    {:name "Fold", :extends "InRange"
-    :description "Fold a signal outside given thresholds."
+    :summary "Fold a signal outside given thresholds."
     :doc "Folds input wave to within the lo and hi thresholds. This
          differs from the ugen fold2 in that it allows one to
          set both low and high thresholds."}
 
    {:name "Clip"
-    :description "Clip a signal outside given thresholds."
+    :summary "Clip a signal outside given thresholds."
     :doc "Clip a signal outside given thresholds. This differs from the
           ugen clip2 in that it allows one to set both low and high
           thresholds."
@@ -481,20 +480,20 @@
     :default-rate :kr}
 
    {:name "Wrap", :extends "InRange"
-    :description "Wrap a signal outside given thresholds."
+    :summary "Wrap a signal outside given thresholds."
     :doc "Wraps input wave to the low and high thresholds. This differs
          from the ugen wrap2 in that it allows one to set both
          low and high thresholds." }
 
    {:name "Schmidt", :extends "InRange"
-    :description "Schmidt trigger"
+    :summary "Schmidt trigger"
     :doc "Outout one when signal greater than high, and zero when lower
          than low." }
 
    ;; TODO maybe allow a rect datatype as arg
    ;;      and write init function to handle it
    {:name "InRect"
-    :description "Test if a point is within a given rectangle."
+    :summary "Test if a point is within a given rectangle."
     :doc "Outputs one if the 2d coordinate of x,y input values falls
           inside a rectangle, else zero"
     :args [{:name "x"
@@ -520,7 +519,7 @@
     :default-rate :kr}
 
    {:name "MostChange"
-    :description "Output most changed"
+    :summary "Output most changed"
     :doc "output whichever signal changed the most"
     :args [{:name "a"
             :doc "first input"
@@ -532,11 +531,11 @@
     :default-rate :kr}
 
    {:name "LeastChange", :extends "MostChange"
-    :description "Output least changed"
+    :summary "Output least changed"
     :doc "output whichever signal changed the least"}
 
    {:name "LastValue"
-    :description "Output the last value before the input changed"
+    :summary "Output the last value before the input changed"
     :doc "Output the last value before the input changed by a threshold
           of diff"
     :args [{:name "in",
