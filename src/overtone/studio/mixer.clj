5b652a9c25e1c352359b03f396bc7e66b175ddb8
Sam Aaron <samaaron@gmail.com>
2012-10-21 19:27:38 +0100

    Fix bug in recording functionality. Fixes #166

--- src/overtone/studio/mixer.clj
+++ src/overtone/studio/mixer.clj
@@ -154,8 +154,8 @@
   (let [path (resolve-tilde-path path)
         bs   (apply buffer-stream path args)
         rec  (master-recorder :target (foundation-monitor-group) bs)]
-    (swap! studio* :recorder {:rec-id rec
-                              :buf-stream bs})
+    (swap! studio* assoc :recorder {:rec-id rec
+                                    :buf-stream bs})
     :recording-started))
 
 (defn recording-stop
