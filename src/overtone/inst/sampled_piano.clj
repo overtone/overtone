1b31960c03cb34621483d183a652fe27f78ddfe1
Sam Aaron <samaaron@gmail.com>
2012-12-08 23:28:06 +0000

    move sampled piano samples to separate piano sample ns for piano samples which store samples of pianos

--- src/overtone/inst/sampled_piano.clj
+++ src/overtone/inst/sampled_piano.clj
@@ -1,11 +1,11 @@
 (ns overtone.inst.sampled-piano
   (:use [overtone.core]
-        [overtone.synth.sampled-piano :only [sampled-piano-index-buffer]]))
+        [overtone.samples.piano :only [index-buffer]]))
 
 (definst sampled-piano
   [note 60 level 1 rate 1 loop? 0
    attack 0 decay 1 sustain 1 release 0.1 curve -4 gate 1]
-  (let [buf (index:kr (:id sampled-piano-index-buffer) note)
+  (let [buf (index:kr (:id index-buffer) note)
         env (env-gen (adsr attack decay sustain release level curve)
                      :gate gate
                      :action FREE)]
