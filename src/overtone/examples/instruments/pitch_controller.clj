22fa083fe75588d3c1142c1313a4fbc1538a358f
Sam Aaron <samaaron@gmail.com>
2012-12-09 00:45:39 +0000

    Add simple mic/input pitch-controlled synth example

---
+++ src/overtone/examples/instruments/pitch_controller.clj
@@ -0,0 +1,15 @@
+(ns overtone.examples.instruments.pitch-controller
+  (:use overtone.live))
+
+;; Fire off this synth and connect an instrument/mic.
+;; The pitch will be detected and used to control the saw waves
+
+(defsynth pitch-controlled-saws
+  [out-bus 0]
+  (let [p   (pitch (sound-in) )
+        p   (/ p 4)
+        p   (lag p 1)]
+    (out out-bus (saw [p (+ p (* p 0.01))]))))
+
+;;(pitch-controlled-saws)
+;;(stop)
