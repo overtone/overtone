990f9bc6da0233605f986851d41e9bc4a915d469
Sam Aaron <samaaron@gmail.com>
2012-12-12 19:25:37 +0000

    add range-lin cgen. Fixes #142

--- src/overtone/sc/cgens/line.clj
+++ src/overtone/sc/cgens/line.clj
@@ -17,3 +17,13 @@
   (:kr (let [scale (/ (- dsthi dstlo) (- srchi srclo))
              offset (- dstlo (* scale srclo))]
          (mul-add in scale offset))))
+
+(defcgen range-lin
+  "Map ugens with default range linearly to another"
+  [in    {:default 0.0 :doc "Input to convert (should have range -1 to 1)"}
+   dstlo {:default 1.0 :doc "Lower limit of output range"}
+   dsthi {:default 2.0 :doc "Upper limit of output range"}]
+  "Linearly maps input signal with expected range of -1 to 1 to the
+   specified range from dstlo to dsthi."
+  (:ar (lin-lin:ar in -1 1 dstlo dsthi))
+  (:kr (lin-lin:kr in -1 1 dstlo dsthi)))
