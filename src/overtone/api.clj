8da78ff1fa8126fa99e5eaf43fd65f6d7d555a9e
Sam Aaron <samaaron@gmail.com>
2012-12-13 19:37:14 +0000

    Remove trigger ns - use the event system instead.

--- src/overtone/api.clj
+++ src/overtone/api.clj
@@ -6,7 +6,7 @@
             [overtone version midi osc speech]
             [overtone.algo chance scaling trig]
             [overtone.sc bindings buffer bus envelope example info
-                         ugens defcgen node sample server synth trigger
+                         ugens defcgen node sample server synth
                          foundation-groups]
             [overtone.sc.cgens oscillators demand mix io buf-io env tap
                                line freq beq-suite berlach bhob]
@@ -34,7 +34,6 @@
    'overtone.sc.sample
    'overtone.sc.server
    'overtone.sc.synth
-   'overtone.sc.trigger
    'overtone.sc.ugens
    'overtone.sc.defcgen
    'overtone.sc.foundation-groups
