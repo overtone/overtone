73ae3f28fded78aff085a655792a43741edbd37b
Sam Aaron <samaaron@gmail.com>
2012-12-29 21:07:02 +0000

    tidy docstrings

--- src/overtone/sc/machinery/allocator.clj
+++ src/overtone/sc/machinery/allocator.clj
@@ -1,5 +1,7 @@
-(ns
-    ^{:doc "ID allocator system. Used to return new unique integer IDs in a threadsafe manner. IDs may be freed and therefore reused. Allows action fns to be executed in synchronisation with allocation and deallocation."
+(ns ^{:doc "ID allocator system. Used to return new unique integer IDs
+            in a threadsafe manner. IDs may be freed and therefore
+            reused. Allows action fns to be executed in synchronisation
+            with allocation and deallocation."
       :author "Sam Aaron"}
   overtone.sc.machinery.allocator
   (:use [overtone.sc defaults]
@@ -26,10 +28,11 @@
    :control-bus  (ref (mk-bitset (sc-arg-default :max-control-bus)))})
 
 (defn- fill-gaps
-  "Returns a new vector similar to bs except filled with with size consecutive vals from idx
+  "Returns a new vector similar to bs except filled with with size
+   consecutive vals from idx
 
-  example: (fill with 3 from idx 1 for 2 vals)
-  (fill-gaps [1 0 0 0 1] 1 2 3) ;=> [1 3 3 0 1]"
+   example: (fill with 3 from idx 1 for 2 vals)
+   (fill-gaps [1 0 0 0 1] 1 2 3) ;=> [1 3 3 0 1]"
   [bs idx size val]
   (loop [bs bs
          idx idx
@@ -39,8 +42,8 @@
       bs)))
 
 (defn- find-gap
-  "Returns index of the first gap in vector bs with specified size where gap is
-  defined as a falsey value.
+  "Returns index of the first gap in vector bs with specified size where
+   gap is defined as a falsey value.
 
   example:
   (find-gap [true false true false false false true] 3) ;=> 3"
@@ -57,8 +60,8 @@
 (defonce action-fn-executor* (agent nil))
 
 (defn- execute-action-fn
-  "Execute action-fn and catch all exceptions - outputting them to the error
-  log. All actions are executed in sequence."
+  "Execute action-fn and catch all exceptions - outputting them to the
+   error log. All actions are executed in sequence."
   [action-fn caller-name]
   (send action-fn-executor* (fn [_]
                               (try
@@ -68,14 +71,15 @@
                                 (finally nil)))))
 
 (defn alloc-id
-  "Allocate a new ID for the type corresponding to key. Takes an optional
-  action-fn which it will evaluate in transaction with the allocation of the id.
-  Therefore there is no possibility of interleaving concurrent allocation of ids
-  and the execution of associated action-fns. Execution of action-fn is also
-  synchronised with the execution of free-id action-fns. Action-fn takes one
-  param - the newly allocated id.
+  "Allocate a new ID for the type corresponding to key. Takes an
+   optional action-fn which it will evaluate in transaction with the
+   allocation of the id.  Therefore there is no possibility of
+   interleaving concurrent allocation of ids and the execution of
+   associated action-fns. Execution of action-fn is also synchronised
+   with the execution of free-id action-fns. Action-fn takes one param -
+   the newly allocated id.
 
-  Returns newly allocated id."
+   Returns newly allocated id."
   ([k] (alloc-id k 1 nil))
   ([k size] (alloc-id k size nil))
   ([k size action-fn]
@@ -92,13 +96,14 @@
 (defonce _root-group_ (alloc-id :node))
 
 (defn free-id
-  "Free the ID of type key. Takes an optional action-fn which it will evaluate
-  in transaction with the freeing of the id. (Evaluation happens within an agent
-  so there's no need to worry about the transaction retrying multiple times).
-  Therefore there is no possibility of interleaving concurrent freeing of ids
-  and execution of associated action-fns. Execution of action-fn is also
-  synchronised with the execution of alloc-id action-fns (they all use the same
-  agent)."
+  "Free the ID of type key. Takes an optional action-fn which it will
+   evaluate in transaction with the freeing of the id. (Evaluation
+   happens within an agent so there's no need to worry about the
+   transaction retrying multiple times).  Therefore there is no
+   possibility of interleaving concurrent freeing of ids and execution
+   of associated action-fns. Execution of action-fn is also synchronised
+   with the execution of alloc-id action-fns (they all use the same
+   agent)."
   ([k id] (free-id k id 1 nil))
   ([k id size] (free-id k id size nil))
   ([k id size action-fn]
