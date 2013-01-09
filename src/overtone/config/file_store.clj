8199c611836a6e0ee9a61082607b29e6c1d344a4
Sam Aaron <samaaron@gmail.com>
2013-01-09 12:06:59 +0000

    formatting and spelling corrections

--- src/overtone/config/file_store.clj
+++ src/overtone/config/file_store.clj
@@ -1,11 +1,12 @@
-(ns
-    ^{:doc "Provides a simple key/value store which will automatically persist to a file on disk. The file is serialized clojure code which can be easily edited as text."
+(ns ^{:doc "Provides a simple key/value store which will automatically
+            persist to a file on disk. The file is serialized clojure
+            code which can be easily edited as text."
       :author "Jeff Rose, Kevin Neaton"}
   overtone.config.file-store
   (:import [java.io FileOutputStream FileInputStream])
   (:use [clojure.pprint]))
 
-;; This should be temporay...
+;; This should be temporary...
 (def storage (constantly :file))
 
 ;; Store interface:
@@ -26,11 +27,12 @@
     (read-string (slurp file))))
 
 (defn live-file-store
-  "Uses the file-store located at the given path. Restores the file-store if it
-  already exists and persists any changes to disk as they occur.
+  "Uses the file-store located at the given path. Restores the
+   file-store if it already exists and persists any changes to disk as
+   they occur.
 
-  (def data (ref {}))
-  (live-store data \"~/.app-data\")"
+   (def data (ref {}))
+   (live-store data \"~/.app-data\")"
   [reference path & [initial-value]]
   (dosync
    (ref-set reference (or initial-value
