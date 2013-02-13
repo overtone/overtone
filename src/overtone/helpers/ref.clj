4091951c32b23b1fd73af1665c8a656576b8abf6
Sam Aaron <samaaron@gmail.com>
2013-02-13 22:24:11 +0000

    add reset-returning-prev! ref helper fn

--- src/overtone/helpers/ref.clj
+++ src/overtone/helpers/ref.clj
@@ -16,3 +16,16 @@
       (if success?
         [old-val new-val]
         (recur)))))
+
+(defn reset-returning-prev!
+  "Similar to reset! except returns vector containing the previous and new values
+
+  (def a (atom 0))
+  (reset-returning-prev! a 1) ;=> [0 1]"
+  [atom val]
+  (loop []
+    (let [old-val @atom
+          success? (compare-and-set! atom old-val val)]
+      (if success?
+        [old-val val]
+        (recur)))))
