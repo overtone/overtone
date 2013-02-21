71c7facc1c97186d211835ef25e0579d8d077293
Sam Aaron <samaaron@gmail.com>
2013-02-21 23:12:48 +0000

    Remove old ns use cases. Fixes #225

--- src/overtone/inst/io.clj
+++ src/overtone/inst/io.clj
@@ -1,6 +1,5 @@
 (ns overtone.inst.io
-  (:use [overtone.sc gens envelope]
-        [overtone.studio mixer]))
+  (:use [overtone.core]))
 
 (definst mic [amp 1]
   (let [src (in (num-output-buses:ir))]
