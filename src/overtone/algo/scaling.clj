74001a7136b7ebc20ef1bd60c93e90cca6b186a4
Sam Aaron <samaaron@gmail.com>
2013-02-21 23:14:17 +0000

    remove limit and ordering restrictions on scale-range

--- src/overtone/algo/scaling.clj
+++ src/overtone/algo/scaling.clj
@@ -34,16 +34,9 @@
    f (x) = --------------------------------  + out-min
                     in-max - in-min
 
-
-  The result will be limited to the specified out range. So if the
-  result of f(x) is greater than out-max it is reduced to out-max.
-
-  in-min must be less than in-max and out-min must be less than
-  out-max"
+"
 
   [x in-min in-max out-min out-max]
-  (letfn [(scale [x] (+ (/ (* (- out-max out-min) (- x in-min)) (- in-max in-min)) out-min))]
-    (let [result (scale x)
-          result (if (< result out-min) out-min result)
-          result (if (> result out-max) out-max result)]
-      result)))
+  (+ (/ (* (- out-max out-min) (- x in-min))
+        (- in-max in-min))
+     out-min))
