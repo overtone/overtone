6556d0dff984e653f190d514c2f3ed594c9e0a11
Sam Aaron <samaaron@gmail.com>
2013-04-05 08:38:44 +0100

    Add amp param to sampled-piano synth

--- src/overtone/synth/sampled_piano.clj
+++ src/overtone/synth/sampled_piano.clj
@@ -4,9 +4,9 @@
 
 (defsynth sampled-piano
   [note 60 level 1 rate 1 loop? 0
-   attack 0 decay 1 sustain 1 release 0.1 curve -4 gate 1 out-bus 0]
+   attack 0 decay 1 sustain 1 release 0.1 curve -4 gate 1 out-bus 0 amp 1]
   (let [buf (index:kr (:id index-buffer) note)
         env (env-gen (adsr attack decay sustain release level curve)
                      :gate gate
                      :action FREE)]
-    (out out-bus (* env (scaled-play-buf 2 buf :level level :loop loop? :action FREE)))))
+    (out out-bus (* env amp (scaled-play-buf 2 buf :level level :loop loop? :action FREE)))))
