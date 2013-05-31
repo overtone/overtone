5dada8e31a161295d7c20fbfcece87f765c34b87
Sam Aaron <samaaron@gmail.com>
2013-05-31 16:02:39 +0300

    improve docstring for at macro

--- src/overtone/sc/server.clj
+++ src/overtone/sc/server.clj
@@ -50,15 +50,34 @@
   (= :external (:connection-type (connection-info))))
 
 (defmacro at
-  "All messages sent within the body will be sent in the same
-  timestamped OSC bundle.  This bundling is thread-local, so you don't
-  have to worry about accidentally scheduling packets into a bundle
-  started on another thread.
+  "Schedule server communication - specify that communication messages
+   execute on the server at a specific time in the future:
 
-  Warning, all liveness and node blocking when not ready checks and
-  functionality is disabled within the context of this macro. This means
-  that it's perfectly possible for the node you wish to control to either have
-  been since terminated or not had time to be initialised."
+   ;; control synth foo to change :freq to 150
+   ;; one second from now:
+   (at (+ (now) 1000) (ctl foo :freq 150))
+
+   Only affects code that communicates with the server using OSC
+   messaging i.e. synth triggering and control. All code in the body of
+   the at macro is executed immediately. Any OSC messages which are
+   triggered as a result of executing the body are not immediately sent
+   but are instead captured and then sent in a single OSC bundle with
+   the specified timestamp once the body has completed. The server then
+   stores these bundles and executes them at the specified time. This
+   allows you to schedule the triggering and control of synths for
+   specific times.
+
+   The bundling is thread-local, so you don't have to worry about
+   accidentally scheduling packets into a bundle started on another
+   thread.
+
+   Be careful not to confuse at with apply-at and apply-by which
+   directly affect Clojure code.
+
+   Warning, all liveness and 'node blocking when not ready' checks are
+   disabled within the context of this macro. This means that it will
+   fail silently if a server node you wish to control either has been
+   since terminated or not had time to be initialised."
   [time-ms & body]
   `(with-inactive-modification-error :silent
      (without-node-blocking
