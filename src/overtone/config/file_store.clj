fd0ac110052021e70513b7f54196e3405fd3e760
Sam Aaron <samaaron@gmail.com>
2013-08-15 18:30:50 +0100

    Further clean up file-store
    * Don't throw exception when trying to read from a store which doesn't exist
    * Initialise store with an empty map if it doesn't yet exist

--- src/overtone/config/file_store.clj
+++ src/overtone/config/file_store.clj
@@ -3,9 +3,11 @@
             code which can be easily edited as text."
       :author "Jeff Rose, Kevin Neaton"}
   overtone.config.file-store
+  (:use [clojure.pprint])
+  (:require [overtone.helpers.file :as file-helpers])
   (:import [java.io FileOutputStream FileInputStream])
-  (:use [clojure.pprint]))
 
+)
 ;; This should be temporary...
 (def storage (constantly :file))
 
@@ -24,17 +26,25 @@
 (defmethod read-file-store :file
   [path]
   (locking F-LOCK
-    (with-open [file (FileInputStream. path)]
-      (read-string (slurp file)))))
+    (when (file-helpers/path-exists? path)
+      (read-string (slurp path)))))
+
+(defmethod read-file-store :file
+  [path]
+  (locking F-LOCK
+    (when (file-helpers/path-exists? path)
+      (read-string (slurp path)))))
 
 (defn live-file-store
   "Returns an atom representing a file-store map located at the given
    path. Persists any changes to disk as they occur within the atom.
 
+   Creates file if doesn't exist and initialises value with empty map.
+
    (def data (live-file-store \"~/.app-data\"))
    (swap! data assoc :foo 1) ;=> persists key val pair to ~/.app-data"
   [path]
-  (let [store-atom (atom (read-file-store path))]
+  (let [store-atom (atom (or (read-file-store path) {}))]
     (add-watch store-atom ::live-file-store
                (fn [k r old-state new-state]
                  (when-not (= old-state new-state)
