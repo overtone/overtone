75a48ec2b01a555ace452167195fbf4babfcecf9
Sam Aaron <samaaron@gmail.com>
2013-09-27 19:51:58 -0400

    add ns for monitoring synths

---
+++ src/overtone/synth/monitor.clj
@@ -0,0 +1,11 @@
+(ns
+    ^{:doc "Useful monitoring synths"
+      :author "Sam Aaron"}
+  overtone.synth.monitor
+  (:use [overtone.core]))
+
+(defsynth mono-audio-bus-level [in-a-bus 0 out-c-bus 0]
+  (let [sig   (in:ar in-a-bus 1)
+        level (amplitude sig)
+        level (lag-ud level 0.1 0.3)]
+    (out:kr out-c-bus [(a2k level)])))
