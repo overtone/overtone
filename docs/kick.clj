75be932ca7dd5d6aeebc3da6295261883fa7c064
Jack Rusher <jack@rusher.com>
2014-08-14 11:16:46 +0200

    Update kick.clj
    An API change seems to have broken this example, as sin-osc no longer takes a third (amp) parameter.

--- docs/kick.clj
+++ docs/kick.clj
@@ -11,7 +11,7 @@
 ;; a low tone.
 (defsynth kick [amp 0.5 decay 0.6 freq 65]
   (let [env (env-gen (perc 0 decay) 1 1 0 1 FREE)
-        snd (sin-osc freq (* Math/PI 0.5) amp)]
+        snd (sin-osc freq (* Math/PI 0.5))]
     (out 0 (pan2 (* snd env) 0))))
 
 ;; This can be triggered using the name as a function call:
@@ -20,7 +20,7 @@
 ;; A shorter variation using definst which allows you to leave out the out and pan ugens,
 ;; which get added by default to synths that are rooted by an audio rate ugen that isn't out.
 (definst kick [amp 0.5 decay 0.6 freq 65]
-  (* (sin-osc freq (* Math/PI 0.5) amp)
+  (* (sin-osc freq (* Math/PI 0.5))
      (env-gen (perc 0 decay) 1 1 0 1 FREE)))
 
 ;; Similarly this is also triggered using the name as a function call:
