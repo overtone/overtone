3c7ca79c9b758fc3b4bd61aa0598d703d9045fa5
Andréas Kündig <andreas.kuendig@gmx.ch>
2022-06-06 13:29:31 +0200

    Fix window paths to allow downloading samples (#487)
    * avoid using os-specific file separator in store
    
    * remove url params from asset path

--- src/overtone/config/store.clj
+++ src/overtone/config/store.clj
@@ -5,7 +5,7 @@
   (:use [overtone.config.file-store]
         [overtone.helpers.string :only [capitalize]]
         [overtone.helpers.system :only [get-os system-user-name]]
-        [overtone.helpers.file :only [mkdir! file-exists? path-exists? mv!]]
+        [overtone.helpers.file :only [mkdir! file-exists? path-exists? mv! mk-path]]
         [overtone version]
         [clojure.java.io :only [delete-file]]))
 
@@ -69,19 +69,19 @@
   @live-store)
 
 (def OVERTONE-DIRS
-  (let [root   (str (System/getProperty "user.home") "/.overtone")
-        log    (str root "/log")
-        assets (str root "/assets")
-        speech (str root "/speech")]
+  (let [root   (mk-path (System/getProperty "user.home") ".overtone")
+        log    (mk-path root "log")
+        assets (mk-path root "assets")
+        speech (mk-path root "speech")]
       {:root root
        :log log
        :assets assets
        :speech speech}))
 
-(def OVERTONE-CONFIG-FILE     (str (:root   OVERTONE-DIRS) "/config.clj"))
-(def OVERTONE-USER-STORE-FILE (str (:root   OVERTONE-DIRS) "/user-store.clj"))
-(def OVERTONE-ASSETS-FILE     (str (:assets OVERTONE-DIRS) "/assets.clj"))
-(def OVERTONE-LOG-FILE        (str (:log    OVERTONE-DIRS) "/overtone.log"))
+(def OVERTONE-CONFIG-FILE     (mk-path (:root   OVERTONE-DIRS) "config.clj"))
+(def OVERTONE-USER-STORE-FILE (mk-path (:root   OVERTONE-DIRS) "user-store.clj"))
+(def OVERTONE-ASSETS-FILE     (mk-path (:assets OVERTONE-DIRS) "assets.clj"))
+(def OVERTONE-LOG-FILE        (mk-path (:log    OVERTONE-DIRS) "overtone.log"))
 
 (defn- ensure-dir-structure
   []
@@ -121,7 +121,7 @@
 
 (defonce __MOVE-OLD-ROOT-DIR__
   (let [root (:root OVERTONE-DIRS)]
-      (when (path-exists? (str root "/config"))
+      (when (path-exists? (mk-path root "config"))
         (println "Warning - old config directory detected. Moved to ~/.overtone-old and replaced with new, empty config.")
         (mv! root (str root "-old")))))
 
