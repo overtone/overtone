502dd7c4e2c9beeb536020070ba7d9ce83da3e95
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-10-22 23:05:10 -0500

    return entire scale regardless of length

--- test/overtone/music/pitch_test.clj
+++ test/overtone/music/pitch_test.clj
@@ -1,5 +1,5 @@
 (ns overtone.music.pitch-test
-  (:use overtone.music.pitch
+  (:use [overtone.music.pitch :as sut]
         clojure.test))
 
 (deftest invert-chord-works-properly
@@ -10,3 +10,13 @@
 (deftest chord-inversion-is-correct
   (is (= (chord :F3 :major 1) '(57 60 65)))
   (is (= (chord :F3 :major 2) '(60 65 69))))
+
+(deftest scale-test
+  (is (= [60 62 64 65 67 69 71 72]
+         (sut/scale :c4 :major)))
+  (is (= [60 62 63 65 67 68 70 72]
+         (sut/scale :c4 :minor)))
+  (is (= [60 61 62 63 64 65 66 67 68 69 70 71 72]
+         (sut/scale :c4 :chromatic)))
+  (is (= [2 4 7 9 11 14]
+         (sut/scale :d-1 :pentatonic))))
